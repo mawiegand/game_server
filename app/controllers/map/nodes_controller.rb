c5d0cb81a97e686fd1a6f49b8b6ea47ebd802133
Sascha Lange <sascha77@googlemail.com>
2012-03-05 15:48:14 +0100

    added a few comments regarding the actions available to the API

--- app/controllers/map/nodes_controller.rb
+++ app/controllers/map/nodes_controller.rb
@@ -1,16 +1,23 @@
 class Map::NodesController < ApplicationController
+  
+  # TODO: define what's accessible by the API and what's accessible by the website (admin area)
+  # TODO: define accessibility and readability of attributes (per role)
+  # TODO: a few methods should need authentication (e.g. update (API) and all admin functions)
+  
   # GET /map/nodes
-  # GET /map/nodes.json
+  # TODO: block this for the API
   def index
     @map_nodes = Map::Node.paginate(:page => params[:page], :per_page => 64)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @map_nodes }
     end
   end
 
-  # Shows individual nodes. There are two possibilities for addressing nodes:
+  # Shows individual nodes. Does not need any authentication; the map structure is public and
+  # therefore world-readable.
+  # 
+  # There are two possibilities for addressing nodes:
   # 1. by their id (auto_increment assigned on node creation) or
   # 2. by their quad-tree path (e.g. nodes/qt0101 translates to the forth-level node that 
   #    is the upper-left, upper-rigth, upper-left, upper-right child of the root node.
@@ -45,7 +52,7 @@ class Map::NodesController < ApplicationController
   end
 
   # GET /map/nodes/new
-  # GET /map/nodes/new.json
+  # TODO: block this for the API
   def new
     @map_node = Map::Node.new
 
@@ -61,7 +68,7 @@ class Map::NodesController < ApplicationController
   end
 
   # POST /map/nodes
-  # POST /map/nodes.json
+  # TODO: block this for the API
   def create
     @map_node = Map::Node.new(params[:map_node])
 
@@ -93,7 +100,7 @@ class Map::NodesController < ApplicationController
   end
 
   # DELETE /map/nodes/1
-  # DELETE /map/nodes/1.json
+  # TODO: block this for the API
   def destroy
     @map_node = Map::Node.find(params[:id])
     @map_node.destroy
