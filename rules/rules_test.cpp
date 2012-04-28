b4c9407b8746b9b53f1bcf6b5e8e7b2681d58711
salange <sascha@5dlab.com>
2012-04-28 20:17:35 +0200

    c++ units, test program

---
+++ rules/rules_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "rules.h"
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{	
+	Rules* rules = Rules::theRules();
+	
+	char buf[1000];
+	
+	for (int i=0; i < MAX_UNIT; i++) {	
+		cout << rules->unitTypes[i]->toString(buf, 1000) << endl;
+	}
+	
+	return 0;
+}
