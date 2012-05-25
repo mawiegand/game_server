2c1ec4ebb29ff261658fffd6a9617d626f917121
Your Name <p@trick-fox.de>
2012-05-25 14:49:41 +0200

    get settlements and slots of settlements  of current character only

--- app/models/settlement/settlement.rb
+++ app/models/settlement/settlement.rb
@@ -1,5 +1,7 @@
 class Settlement::Settlement < ActiveRecord::Base
 
-  belongs_to :owner, :class_name => "Fundamental::Character", :foreign_key => "owner_id"  
+  belongs_to :owner, :class_name => "Fundamental::Character", :foreign_key => "owner_id"
+  
+  has_many :slots, :class_name => "Settlement::Slot", :foreign_key => "settlement_id"
 
 end
--- app/models/settlement/slot.rb
+++ app/models/settlement/slot.rb
@@ -1,2 +1,5 @@
 class Settlement::Slot < ActiveRecord::Base
+  
+  belongs_to :settlement, :class_name => "Settlement::Settlement", :foreign_key => "settlement_id"
+  
 end
