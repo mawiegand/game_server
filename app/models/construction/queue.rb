a99afa165ec1fbf6469abcfca8cc4bb1626974b2
Your Name <p@trick-fox.de>
2012-05-30 02:53:32 +0200

    bugfix

--- app/models/construction/queue.rb
+++ app/models/construction/queue.rb
@@ -3,6 +3,6 @@ class Construction::Queue < ActiveRecord::Base
   belongs_to :settlement,  :class_name => "Settlement::Settlement",  :foreign_key => "settlement_id", :inverse_of => :queues 
   
   has_many   :jobs,        :class_name => "Construction::Job",       :foreign_key => "queue_id",      :inverse_of => :jobs
-  has_many   :active_jobs, :class_name => "Construction::ActiveJob", :foreign_key => "queue_id",      :inverse_of => :active_jobs
+  has_many   :active_jobs, :class_name => "Construction::ActiveJob", :foreign_key => "queue_id",      :inverse_of => :queue
   
 end
