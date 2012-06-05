7fe129a216889d0b063e0c5515ecc2a1dc8d9e31
Your Name <p@trick-fox.de>
2012-06-05 15:58:47 +0200

    update timestamp of queue if containing job is updated

--- app/models/construction/job.rb
+++ app/models/construction/job.rb
@@ -4,7 +4,7 @@ class Construction::Job < ActiveRecord::Base
   
   has_one    :active_job,  :class_name => "Construction::ActiveJob", :foreign_key => "job_id",    :inverse_of => :job
   
-  belongs_to :queue,       :class_name => "Construction::Queue",     :foreign_key => "queue_id",  :inverse_of => :jobs,  :counter_cache => true
+  belongs_to :queue,       :class_name => "Construction::Queue",     :foreign_key => "queue_id",  :inverse_of => :jobs,  :counter_cache => true, :touch => true
   belongs_to :slot,        :class_name => "Settlement::Slot",        :foreign_key => "slot_id",   :inverse_of => :jobs
   
   def active?
