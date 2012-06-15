35e92d67fc15985aa4cfff935d950967cec38042
Your Name <p@trick-fox.de>
2012-06-15 15:04:41 +0200

    resolved merging conflict

--- app/models/training/queue.rb
+++ app/models/training/queue.rb
@@ -2,29 +2,6 @@ class Training::Queue < ActiveRecord::Base
   
   belongs_to  :settlement,  :class_name => "Settlement::Settlement",  :foreign_key => "settlement_id", :inverse_of => :training_queues
 
-  before_save :update_speed
-
-  def add_speedup(origin_type, delta)
-    if origin_type    == :building
-      self.speedup_buildings = self.speedup_buildings + delta
-    elsif origin_type == :sciences
-      self.speedup_sciences = self.speedup_sciences + delta
-    elsif origin_type == :alliance
-      self.speedup_alliance = self.speedup_alliance + delta
-    elsif origin_type == :effects
-      self.speedup_effects = self.speedup_effects + delta
-    else 
-      raise InternalServerError.new('Could not add speedup bonus of type #{origin_type.to_s}.');
-    end
-  end
-
-  protected
-  
-    def update_speed
-      sum = 1.0 + self.speedup_buildings + self.speedup_sciences + self.speedup_alliance + self.speedup_effects
-      self.speed = sum < 0.00001 ? 0.00001 : sum
-    end
-
   has_many   :jobs,        :class_name => "Training::Job",       :foreign_key => "queue_id",      :inverse_of => :queue,   :order => 'position ASC'
   has_many   :active_jobs, :class_name => "Training::ActiveJob", :foreign_key => "queue_id",      :inverse_of => :queue
   
@@ -98,7 +75,7 @@ class Training::Queue < ActiveRecord::Base
   end
 
   protected
-
+  
   def create_event_for_job(active_job)
     #create entry for event table
     event = active_job.build_event(
