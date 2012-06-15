fac7a3867d1c55addda35ef40f30158d7c9801dd
Sascha Lange <sascha77@googlemail.com>
2012-06-15 12:53:47 +0200

    training_queues association

--- app/models/training/queue.rb
+++ app/models/training/queue.rb
@@ -1,2 +1,5 @@
 class Training::Queue < ActiveRecord::Base
+  
+  belongs_to :settlement,  :class_name => "Settlement::Settlement",  :foreign_key => "settlement_id", :inverse_of => :training_queues
+
 end
