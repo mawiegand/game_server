3161f5c00edd0930ea3d5be52db6eb7835da49d8
Your Name <p@trick-fox.de>
2012-06-15 12:56:05 +0200

    set jobs_count to 0 before setting column to 'not null'

--- db/migrate/20120615100618_change_jobs_count_column_in_construction_queue.rb
+++ db/migrate/20120615100618_change_jobs_count_column_in_construction_queue.rb
@@ -1,5 +1,10 @@
 class ChangeJobsCountColumnInConstructionQueue < ActiveRecord::Migration
   def up
+   Construction::Queue.all.each do |q|
+     q.jobs_count = 0 if q.jobs_count.nil?
+     q.save
+   end
+
     change_column  :construction_queues, :jobs_count, :integer, {:default => 0, :null => false}     
   end
 
