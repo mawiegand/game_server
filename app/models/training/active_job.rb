a6726bfe9c9f88fdc6bc46904f605a4903f56b14
Your Name <p@trick-fox.de>
2012-06-15 13:21:09 +0200

    implement models of training resources

--- app/models/training/active_job.rb
+++ app/models/training/active_job.rb
@@ -1,2 +1,8 @@
 class Training::ActiveJob < ActiveRecord::Base
+  
+  belongs_to :job,    :class_name => "Training::Job",   :foreign_key => "job_id",    :inverse_of => :active_job 
+  belongs_to :queue,  :class_name => "Training::Queue", :foreign_key => "queue_id",  :inverse_of => :active_jobs
+  
+  has_one    :event,  :class_name => "Event::Event",    :foreign_key => "local_event_id",  :dependent => :destroy
+ 
 end
