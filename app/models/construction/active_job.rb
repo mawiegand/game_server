a6cfde3f9c05d382b1b2c3561ec703737c29d14e
Your Name <p@trick-fox.de>
2012-06-27 17:27:21 +0200

    bugfix: added condition to models referencing events

--- app/models/construction/active_job.rb
+++ app/models/construction/active_job.rb
@@ -3,6 +3,6 @@ class Construction::ActiveJob < ActiveRecord::Base
   belongs_to :job,    :class_name => "Construction::Job",   :foreign_key => "job_id",    :inverse_of => :active_job 
   belongs_to :queue,  :class_name => "Construction::Queue", :foreign_key => "queue_id",  :inverse_of => :active_jobs
   
-  has_one    :event,  :class_name => "Event::Event",        :foreign_key => "local_event_id",  :dependent => :destroy
+  has_one    :event,  :class_name => "Event::Event",        :foreign_key => "local_event_id",  :dependent => :destroy, :conditions => "event_type = 'construction_active_job'"
 
 end
--- app/models/training/active_job.rb
+++ app/models/training/active_job.rb
@@ -3,7 +3,7 @@ class Training::ActiveJob < ActiveRecord::Base
   belongs_to :job,    :class_name => "Training::Job",   :foreign_key => "job_id",    :inverse_of => :active_job 
   belongs_to :queue,  :class_name => "Training::Queue", :foreign_key => "queue_id",  :inverse_of => :active_jobs
   
-  has_one    :event,  :class_name => "Event::Event",    :foreign_key => "local_event_id",  :dependent => :destroy
+  has_one    :event,  :class_name => "Event::Event",    :foreign_key => "local_event_id",  :dependent => :destroy, :conditions => "event_type = 'training_active_job'"
 
   def create_ticker_event
     #create entry for event table
