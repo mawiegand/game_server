308aee2687224514c5fb1e61cac5a0a94ac02e62
Sascha Lange <sascha77@googlemail.com>
2012-06-27 14:52:59 +0200

    corrected propagation of alliance change

--- app/models/fundamental/resource_pool.rb
+++ app/models/fundamental/resource_pool.rb
@@ -2,8 +2,10 @@ class Fundamental::ResourcePool < ActiveRecord::Base
   
   belongs_to :owner,    :class_name => "Fundamental::Character", :foreign_key => "character_id", :inverse_of => :resource_pool
 
-  before_save :update_global_effects
   before_save :update_resources_on_production_rate_changes
+
+  after_save  :propagate_global_effect_changes
+
   
   # updates resource amounts BUT does NOT save to database itself.
   def update_resource_amount
@@ -61,17 +63,35 @@ class Fundamental::ResourcePool < ActiveRecord::Base
   # adds the given effect to the resource pool.
   # this adds the speedup value to the appropriate global effects
   # value and updates the production rates through an after_save handler.   
-  def add_effect(effect)
+  def add_effect_transaction(effect)
     logger.debug '---> in add_effect'
     logger.debug '---> ' + effect.inspect
+    
+    attribute = resource_type[effect[:resource_id]].to_s()+'_global_effects'
+    amount = effect[:speedup]
+    
+    raise BadRequest.new("could not find effect field when adding effect") unless self.has_attribute?(attribute)
+    raise BadRequest.new("no amount for effect given") if amount.nil?
+    
+    self[attribute] += amount
+    self.save
   end
   
   # removes the given effect from the resource pool.
   # this subtracts the speedup value from the appropriate global effects
   # value and updates the production rates through an after_save handler.   
-  def remove_effect(effect)
+  def remove_effect_transaction(effect)
     logger.debug '---> in remove_effect'
     logger.debug '---> ' + effect.inspect
+    
+    attribute = resource_type[effect[:resource_id]].to_s()+'_global_effects'
+    amount = effect[:speedup]
+    
+    raise BadRequest.new("could not find effect field when removing effect") unless self.has_attribute?(attribute)
+    raise BadRequest.new("no amount for effect given") if amount.nil?
+    
+    self[attribute] -= amount
+    self.save    
   end
   
   protected
@@ -91,9 +111,40 @@ class Fundamental::ResourcePool < ActiveRecord::Base
       true
     end
     
-    # updates the production rate if the global effect changes in this write 
-    def update_global_effects
-      logger.debug '---> in update_global_effects: changed ' + self.changes.inspect
-      #
+    ##########################################################################
+    #
+    #  SPREADING LOCAL CHANGES TO RELATED MODELS 
+    #
+    ##########################################################################
+    
+    # propagate changes to global effects down to settlements. during this
+    # process, changes at settlements will propagate back to this model
+    # (this is quite dangerous...)
+    #
+    # Presently, the method will save each settlement several times; namely one
+    # time for each changed resource-effect. This is ok, as long as we only
+    # change one effect at a time (persently that's given).
+    def propagate_global_effect_changes
+
+      if (!self.character_id.nil? && self.character_id > 0)         # only spread, if there's a resource pool
+        GameRules::Rules.the_rules().resource_types.each do |resource_type|
+          
+          attribute = resource_type[:symbolic_id].to_s()+'_global_effects'
+          attributeSettlement = resource_type[:symbolic_id].to_s()+'_production_bonus_effects'
+          
+          if !self.changes[attribute].nil?
+            change = self.changes[attribute]
+            delta = change[1] - change[0]   # new - old value
+            
+            self.character.settlements.each do |settlement|
+              settlement[attributeSettlement] = settlement[attributeSettlement] + delta
+              settlement.save
+            end
+          end
+        end
+      end
+      
+      true
     end
+    
 end
