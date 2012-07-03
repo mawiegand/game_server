22345ed2b5f908224237554a41fb3f82466f075f
Your Name <p@trick-fox.de>
2012-07-03 23:56:48 +0200

    use consume_ap method to reduce ap

--- app/controllers/action/military/attack_army_actions_controller.rb
+++ app/controllers/action/military/attack_army_actions_controller.rb
@@ -15,11 +15,9 @@ class Action::Military::AttackArmyActionsController < ApplicationController
 
     raise ForbiddenError.new('tried to attack own army') unless attacker.owner_id != defender.owner_id
 
-    raise ForbiddenError.new('not enough action points for attack') unless attacker.ap_present > 0
-    attacker.ap_present -= 1
-
     raise ForbiddenError.new('attacker is already moving or battling') unless attacker.mode === Military::Army::MODE_IDLE || attacker.battle_id.nil?
 
+    attacker.consume_ap                                          # raises an BadAccessError if no aps are available
     Military::Battle.start_fight_between(attacker, defender)     # creates and returns battle, or returns nil, if army was overrun
 
     
