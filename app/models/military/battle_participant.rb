3a43edffecc34ab756221d176ce3bf32a60d4303
Your Name <p@trick-fox.de>
2012-07-19 14:12:39 +0200

    touch battle when changing participants, rounds or factions

--- app/models/military/battle_participant.rb
+++ app/models/military/battle_participant.rb
@@ -1,6 +1,6 @@
 class Military::BattleParticipant < ActiveRecord::Base
 
-  belongs_to :battle,  :class_name => "Military::Battle",        :foreign_key => "battle_id",  :inverse_of => :participants  
+  belongs_to :battle,  :class_name => "Military::Battle",        :foreign_key => "battle_id",  :inverse_of => :participants, :touch => true
   belongs_to :faction, :class_name => "Military::BattleFaction", :foreign_key => "faction_id", :inverse_of => :participants
   belongs_to :army,    :class_name => "Military::Army",          :foreign_key => "army_id",    :inverse_of => :battle_participant
 
