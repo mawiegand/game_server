d6cb39c3d01aebae9b68debd8c93af2dcbe44a93
Sascha Lange <sascha77@googlemail.com>
2012-07-29 21:57:44 +0200

    last login not created

--- app/controllers/backend/dashboard_controller.rb
+++ app/controllers/backend/dashboard_controller.rb
@@ -14,13 +14,13 @@ class Backend::DashboardController < ApplicationController
     @user_stats = {
       active_accounts:          Fundamental::Character.where(['npc != ? AND premium_account = ?', true, true]).count,
       total_accounts:           Fundamental::Character.where(['npc != ?', true]).count,
-      signins_last_hour:        Fundamental::Character.where(['npc != ? AND created_at > ?', true, Time.now - 1.hours]).count,
-      signins_last_eight_hours: Fundamental::Character.where(['npc != ? AND created_at > ?', true, Time.now - 8.hours]).count,
-      signins_last_day:         Fundamental::Character.where(['npc != ? AND created_at > ?', true, Time.now - 1.days]).count,
-      signins_last_week:        Fundamental::Character.where(['npc != ? AND created_at > ?', true, Time.now - 1.weeks]).count,      
+      signins_last_hour:        Fundamental::Character.where(['npc != ? AND last_login_at > ?', true, Time.now - 1.hours]).count,
+      signins_last_eight_hours: Fundamental::Character.where(['npc != ? AND last_login_at > ?', true, Time.now - 8.hours]).count,
+      signins_last_day:         Fundamental::Character.where(['npc != ? AND last_login_at > ?', true, Time.now - 1.days]).count,
+      signins_last_week:        Fundamental::Character.where(['npc != ? AND last_login_at > ?', true, Time.now - 1.weeks]).count,      
     }
     
-    @last_character = Fundamental::Character.order('last_login_at DESC').first
+    @last_character = Fundamental::Character.unscoped.order('last_login_at DESC').first
 
   end
   
