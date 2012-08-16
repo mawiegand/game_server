a15b2edfec7c8218bae6adc2b6ee497f1274c867
Sascha Lange <sascha77@googlemail.com>
2012-08-16 22:09:33 +0200

    track and display resource stats for active users

--- app/models/backend/stat.rb
+++ app/models/backend/stat.rb
@@ -73,27 +73,45 @@ class Backend::Stat < ActiveRecord::Base
     Fundamental::Character.joins(:shop_transactions).where(['npc != ? AND state = ?', true, Shop::Transaction::STATE_CLOSED]).select('fundamental_characters.id').group('fundamental_characters.id').length  
   end  
   
+  def self.resource_stats
+    select_string = nil
+    
+    rules = GameRules::Rules.the_rules
+    rules.resource_types.each do |resource_type| 
+      select_string =  (select_string.blank? ? "" : (select_string + ", ")) + 
+        "SUM(#{resource_type[:symbolic_id]}_amount) AS #{resource_type[:symbolic_id]}_amount_sum, " +
+        "SUM(#{resource_type[:symbolic_id]}_production_rate) AS #{resource_type[:symbolic_id]}_production_rate_sum, " +
+        "MAX(#{resource_type[:symbolic_id]}_amount) AS #{resource_type[:symbolic_id]}_amount_max, " +
+        "MAX(#{resource_type[:symbolic_id]}_production_rate) AS #{resource_type[:symbolic_id]}_production_rate_max "
+    end   
+    
+    r = Fundamental::ResourcePool.joins(:owner).where(['npc != ? AND last_login_at > ?', true, Time.now - Backend::Stat.activity_period]).select(select_string).first    
+  end
+  
   
   def recalc_all_stats
-    self.dnu          = Backend::Stat.num_new_users_last_day
-    self.wnu          = Backend::Stat.num_new_users_last_week
-    self.mnu          = Backend::Stat.num_new_users_last_month
+    self.dnu              = Backend::Stat.num_new_users_last_day
+    self.wnu              = Backend::Stat.num_new_users_last_week
+    self.mnu              = Backend::Stat.num_new_users_last_month
     
-    self.du           = Backend::Stat.num_users_last_day
-    self.wu           = Backend::Stat.num_users_last_week
-    self.mu           = Backend::Stat.num_users_last_month
+    self.du               = Backend::Stat.num_users_last_day
+    self.wu               = Backend::Stat.num_users_last_week
+    self.mu               = Backend::Stat.num_users_last_month
     
-    self.dlu          = Backend::Stat.num_lost_users_last_day
-    self.wlu          = Backend::Stat.num_lost_users_last_week
-    self.mlu          = Backend::Stat.num_lost_users_last_month    
+    self.dlu              = Backend::Stat.num_lost_users_last_day
+    self.wlu              = Backend::Stat.num_lost_users_last_week
+    self.mlu              = Backend::Stat.num_lost_users_last_month    
     
     self.active_users     = Backend::Stat.num_active_users
     self.active_customers = Backend::Stat.num_paying_active_users
 
-    self.total_users     = Backend::Stat.num_users
-    self.total_customers = Backend::Stat.num_paying_users
+    self.total_users      = Backend::Stat.num_users
+    self.total_customers  = Backend::Stat.num_paying_users
 
-    
+    resource_stats = Backend::Stat.resource_stats
+    resource_stats.attributes.each do |k,v|
+      self[k] = v
+    end 
   end
   
 end
