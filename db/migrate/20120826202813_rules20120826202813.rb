260228e385a4a85040538d373239ef2bec2a9998
Patrick <p@trick-fox.de>
2012-08-27 22:30:55 +0200

    bugfix

--- db/migrate/20120826202813_rules20120826202813.rb
+++ db/migrate/20120826202813_rules20120826202813.rb
@@ -1,4 +1,4 @@
-mate class Rules20120826202813 < ActiveRecord::Migration
+class Rules20120826202813 < ActiveRecord::Migration
   def change
     add_column :settlement_settlements,   :settlement_unlock_p2p_trade,   :integer,  {:default=>0}    
   end
