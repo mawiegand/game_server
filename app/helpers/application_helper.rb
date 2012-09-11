07e9492ab02d4dafd1bcbee180fa453ac4f4f818
Sascha Lange <sascha77@googlemail.com>
2012-09-11 16:30:27 +0200

    more debug output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,8 @@ module ApplicationHelper
     if_modified_since = request.env['HTTP_IF_MODIFIED_SINCE']
     
     logger.debug("RESOURCE LAST MODIFIED: #{ last_modified }. IF MODIFIED: #{ if_modified_since }.")
-    logger.debug("MODIFIED COMPARISION: #{ last_modified <= Time.httpdate(if_modified_since) } = #{last_modified} <= #{ Time.httpdate(if_modified_since) }.")   if last_modified && if_modified_since
+    logger.debug("NOT MODIFIED COMPARISION: #{ last_modified <= Time.httpdate(if_modified_since) } = #{last_modified} <= #{ Time.httpdate(if_modified_since) }.")   if last_modified && if_modified_since
+    logger.debug("NOT MODIFIED COMPARISION TIME: #{ last_modified <= Time.httpdate(if_modified_since) } = #{last_modified.to_i} <= #{ Time.httpdate(if_modified_since).to_i }.")   if last_modified && if_modified_since
 
     
     if if_modified_since && last_modified && last_modified <= Time.httpdate(if_modified_since)  
