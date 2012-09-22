b0ac464e71c24a482a7f3edf55c6da53c66eda32
Sascha Lange <sascha77@googlemail.com>
2012-09-22 22:50:54 +0200

    old test case removed

--- test/functional/action/military/attack_army_actions_controller_test.rb
+++ test/functional/action/military/attack_army_actions_controller_test.rb
@@ -6,13 +6,6 @@ class Action::Military::AttackArmyActionsControllerTest < ActionController::Test
     @controller.current_backend_user = backend_users(:staff)  # this is a quick hack to make the scaffolded tests pass. Must be moved to individual tests later.
   end
 
-#  test "should create action_military_attack_army_action" do
-#    assert_difference('Action::Military::AttackArmyAction.count') do
-#      post :create, action_military_attack_army_action: @action_military_attack_army_action.attributes
-#    end
-
-#    assert_redirected_to action_military_attack_army_action_path(assigns(:action_military_attack_army_action))
-#  end
 
 
 end
