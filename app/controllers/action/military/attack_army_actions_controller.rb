2b25fb3b5236c1315f78ec3c4fd8b089b01949fd
Sascha Lange <sascha77@googlemail.com>
2012-10-15 13:46:36 +0200

    use a transaction when creating attack and move commands

--- app/controllers/action/military/attack_army_actions_controller.rb
+++ app/controllers/action/military/attack_army_actions_controller.rb
@@ -6,19 +6,24 @@ class Action::Military::AttackArmyActionsController < ApplicationController
   # POST /action/military/attack_army_actions
   # POST /action/military/attack_army_actions.json
   def create
-    attacker = Military::Army.find(params[:action_military_attack_army_action][:attacker_id])
-    defender = Military::Army.find(params[:action_military_attack_army_action][:defender_id])
+    Military::Army.transaction do  
+      attacker = Military::Army.find(params[:action_military_attack_army_action][:attacker_id])
+      defender = Military::Army.find(params[:action_military_attack_army_action][:defender_id])
+      
+      attacker.lock!
+      defender.lock!
     
-    raise ForbiddenError.new('tried to attack army in different location') unless attacker.location_id == defender.location_id
+      raise ForbiddenError.new('tried to attack army in different location') unless attacker.location_id == defender.location_id
 
-    raise ForbiddenError.new('tried to attack with foreign army')          unless attacker.owner_id == current_character_id 
+      raise ForbiddenError.new('tried to attack with foreign army')          unless attacker.owner_id == current_character_id 
 
-    raise ForbiddenError.new('tried to attack own army')                   if attacker.owner_id == defender.owner_id
+      raise ForbiddenError.new('tried to attack own army')                   if attacker.owner_id == defender.owner_id
 
-    raise ForbiddenError.new('attacker is already moving or fighting')     if attacker.fighting? && !attacker.garrison?  # garrison may attack another army! (presently, because there's no wegerecht)
+      raise ForbiddenError.new('attacker is already moving or fighting')     if attacker.fighting? && !attacker.garrison?  # garrison may attack another army! (presently, because there's no wegerecht)
 
-    attacker.consume_ap         unless attacker.garrison?                                    # raises a BadAccessError if no aps are available
-    Military::Battle.start_fight_between(attacker, defender)     # creates and returns battle, or returns nil, if army was overrun
+      attacker.consume_ap         unless attacker.garrison?                                    # raises a BadAccessError if no aps are available
+      Military::Battle.start_fight_between(attacker, defender)     # creates and returns battle, or returns nil, if army was overrun
+    end
     
     respond_to do |format|
       format.json { render json: {}, status: :ok }
--- app/controllers/action/military/move_army_actions_controller.rb
+++ app/controllers/action/military/move_army_actions_controller.rb
@@ -46,47 +46,52 @@ class Action::Military::MoveArmyActionsController < ApplicationController
   # POST /action/military/move_army_actions
   # POST /action/military/move_army_actions.json
   def create
-    @action_military_move_army_action = Action::Military::MoveArmyAction.new(params[:action_military_move_army_action])
-    army = @action_military_move_army_action.army
-    raise BadRequestError.new('army not found') if army.blank?
-    raise BadRequestError.new('not owner of army') unless army.owner == current_character
-    
-    @action_military_move_army_action.starting_location_id = army.location_id
-    @action_military_move_army_action.starting_region_id = army.region_id
-    @action_military_move_army_action.sender_ip = request.remote_ip  #@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
-    
-    # now fetch and fill the target region id
-    target_location = @action_military_move_army_action.target_location 
-    raise BadRequestError.new('target location does not exist') if target_location.nil?
-    @action_military_move_army_action.target_region_id = target_location.region_id
-    
-    # check whether this movement is possible and allowed (neighbouring positions, owned by current character)
-    role = army.owned_by?(current_character_id) ? :owner : :character # TODO: staff / admin
-    raise BadRequestError.new('Invalid action.') unless @action_military_move_army_action.valid_action?(role)
-        
-    @action_military_move_army_action.army.consume_ap(1)  # consume one action point
-    @action_military_move_army_action.army.target_location_id = @action_military_move_army_action.target_location_id
-    @action_military_move_army_action.army.target_region_id = @action_military_move_army_action.target_region_id
-    @action_military_move_army_action.army.mode = Military::Army::MODE_MOVING # 1: moving?
-    @action_military_move_army_action.army.target_reached_at = DateTime.now.advance(:seconds => GAME_SERVER_CONFIG['movement_duration'] * GAME_SERVER_CONFIG['base_time_factor']) # for first tests, should be 15 minutes in future
+    Action::Military::MoveArmyAction.transaction do
+      @action_military_move_army_action = Action::Military::MoveArmyAction.new(params[:action_military_move_army_action])
+      army = @action_military_move_army_action.army
+      
+      raise BadRequestError.new('army not found') if army.blank?
+      raise BadRequestError.new('not owner of army') unless army.owner == current_character
 
-    if !@action_military_move_army_action.army.save  # save army first; better have no movement action than a movement action without the army being properly set (could result in second movement action)
-      raise BadRequestError.new('could not modify army properly')
-    end
-    if !@action_military_move_army_action.save 
-      raise BadRequestError.new('could not create movement action')
-    end
+      army.lock!  # lock this army now in order to prevent another action to be executed in parallel
     
-    #create entry for event table
-    event = Event::Event.new(
-      character_id: current_character_id,
-      execute_at: @action_military_move_army_action.army.target_reached_at,
-      event_type: "action_military_move_army_action",
-      local_event_id: @action_military_move_army_action.id,
-    )
+      @action_military_move_army_action.starting_location_id = army.location_id
+      @action_military_move_army_action.starting_region_id = army.region_id
+      @action_military_move_army_action.sender_ip = request.remote_ip  #@remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
+    
+      # now fetch and fill the target region id
+      target_location = @action_military_move_army_action.target_location 
+      raise BadRequestError.new('target location does not exist') if target_location.nil?
+      @action_military_move_army_action.target_region_id = target_location.region_id
+    
+      # check whether this movement is possible and allowed (neighbouring positions, owned by current character)
+      role = army.owned_by?(current_character_id) ? :owner : :character # TODO: staff / admin
+      raise BadRequestError.new('Invalid action.') unless @action_military_move_army_action.valid_action?(role)
         
-    if !event.save # this is the final step; this makes sure, something is actually executed
-      raise BadRequestError.new('could not create event')
+      @action_military_move_army_action.army.consume_ap(1)  # consume one action point
+      @action_military_move_army_action.army.target_location_id = @action_military_move_army_action.target_location_id
+      @action_military_move_army_action.army.target_region_id = @action_military_move_army_action.target_region_id
+      @action_military_move_army_action.army.mode = Military::Army::MODE_MOVING # 1: moving?
+      @action_military_move_army_action.army.target_reached_at = DateTime.now.advance(:seconds => GAME_SERVER_CONFIG['movement_duration'] * GAME_SERVER_CONFIG['base_time_factor']) # for first tests, should be 15 minutes in future
+
+      if !@action_military_move_army_action.army.save  # save army first; better have no movement action than a movement action without the army being properly set (could result in second movement action)
+        raise BadRequestError.new('could not modify army properly')
+      end
+      if !@action_military_move_army_action.save 
+        raise BadRequestError.new('could not create movement action')
+      end
+    
+      #create entry for event table
+      event = Event::Event.new(
+        character_id: current_character_id,
+        execute_at: @action_military_move_army_action.army.target_reached_at,
+        event_type: "action_military_move_army_action",
+        local_event_id: @action_military_move_army_action.id,
+      )
+        
+      if !event.save # this is the final step; this makes sure, something is actually executed
+        raise BadRequestError.new('could not create event')
+      end
     end
         
     respond_to do |format|
