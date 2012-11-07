fbdb241fa16ebcc49c1d7db73d8a219de4fa247e
Marcel Wiegand <marcel@bluepanel.org>
2012-11-07 03:39:39 +0100

    fixed missing authentication for platinum offers

--- app/controllers/shop/platinum_offers_controller.rb
+++ app/controllers/shop/platinum_offers_controller.rb
@@ -1,4 +1,9 @@
 class Shop::PlatinumOffersController < ApplicationController
+  layout 'shop'
+
+  before_filter :authenticate
+  before_filter :deny_api, :except => [:show, :index]
+
   # GET /shop/platinum_offers
   # GET /shop/platinum_offers.json
   def index
