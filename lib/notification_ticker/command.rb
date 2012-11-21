dcf66b58f44faf255784bbac9808de048e2b601a
Patrick <p@trick-fox.de>
2012-11-21 16:13:08 +0100

    notification ticker with retention mail handler stub

---
+++ lib/notification_ticker/command.rb
@@ -0,0 +1,78 @@
+begin
+  require 'daemons'
+rescue LoadError
+  raise "You need to add gem 'daemons' to your Gemfile if you wish to use it."
+end
+require 'optparse'
+
+# this daemon's runloop is based on delayed_job https://github.com/collectiveidea/delayed_job/
+
+module NotificationTicker
+  class Command
+    
+    def initialize(args)
+      @options = {
+        :quiet => true,
+        :pid_dir => "#{Rails.root}/tmp/pids",
+        :identifier => 0
+      }
+      
+      parser = OptionParser.new do |opts|
+        opts.banner = "Usage: #{File.basename($0)} [options] start|stop|restart|run"
+
+        opts.on('-h', '--help', 'Show this message') do
+          puts opts
+          exit 1
+        end
+        opts.on('-e', '--environment=NAME', 'Specifies the environment to run this delayed jobs under (test/development/production).') do |e|
+          STDERR.puts "The -e/--environment option has been deprecated and has no effect. Use RAILS_ENV and see http://github.com/collectiveidea/delayed_job/issues/#issue/7"
+        end
+        opts.on('--pid-dir=DIR', 'Specifies an alternate directory in which to store the process ids.') do |dir|
+          @options[:pid_dir] = dir
+        end
+        opts.on('-i', '--identifier=n', 'A numeric identifier for the worker.') do |n|
+          @options[:identifier] = n
+        end
+        opts.on('--sleep-delay N', "Amount of time to sleep when no jobs are found") do |n|
+          @options[:sleep_delay] = n
+        end
+        opts.on('-p', '--prefix NAME', "String to be prefixed to worker process names") do |prefix|
+          @options[:prefix] = prefix
+        end
+      end
+      @args = parser.parse!(args)      
+    end
+   
+    def daemonize
+      dir = @options[:pid_dir]
+      Dir.mkdir(dir) unless File.exists?(dir)
+
+      process_name = "#{File.basename($0)}.#{@options[:identifier]}"
+      run_process(process_name, dir)
+    end
+
+    def run_process(process_name, dir)
+      NotificationTicker::Runloop.before_fork      
+      Daemons.run_proc(process_name, :dir => dir, :dir_mode => :normal, :ARGV => @args) do |*args|
+        $0 = File.join(@options[:prefix], process_name) if @options[:prefix]
+        run process_name
+      end
+    end
+
+    def run(worker_name = nil)
+      Dir.chdir(Rails.root)
+
+      logname = "notification_ticker_#{Rails.env}.log"
+
+      NotificationTicker::Runloop.after_fork      
+      NotificationTicker::Runloop.logger ||= Logger.new(File.join(Rails.root, 'log', logname))
+
+      runloop = NotificationTicker::Runloop.new(@options)
+      runloop.start
+    rescue => e
+     # Rails.logger.fatal e
+      STDERR.puts e.message
+      exit 1
+    end
+  end
+end
---
+++ lib/notification_ticker/retention_mail_handler.rb
@@ -0,0 +1,60 @@
+require 'notification_ticker/runloop'
+
+class NotificationTicker::RetentionMailHandler
+  
+  def runloop 
+    return @runloop
+  end
+  
+  def runloop=(arg)
+    @runloop = arg
+  end
+  
+  def notification_type
+    "retention_mail"
+  end
+  
+  def process
+    
+    # add process code here!
+    
+    runloop.say "Process Notification"
+         
+    
+    # action = Action::Military::MoveArmyAction.find(event.local_event_id)
+    # if action.nil?
+      # runloop.say "Could not find movement action for id #{ event.local_event_id }.", Logger::ERROR
+    # else
+      # runloop.say "Process movement action #{ action.id } moving army #{ action.army_id } from loc #{ action.starting_location_id} in reg #{ action.starting_region_id } to loc #{ action.target_location_id } in reg #{ action.target_region_id }"
+#       
+      # if action.army.nil? 
+        # runloop.say "Could not find army #{ action.army_id }.", Logger::ERROR
+        # return ;
+      # elsif action.army.location_id != action.starting_location_id
+        # runloop.say "Army #{ action.army_id } is at loc #{ action.army.location_id } but it was expected to be at loc #{ action.starting_location_id }.", Logger::ERROR
+        # return ;
+      # end
+# 
+      # target_location = action.army.target_location
+#       
+      # action.army.location_id = action.target_location_id  
+      # action.army.region_id = action.target_region_id  
+      # action.army.target_location = nil
+      # action.army.target_region = nil
+      # action.army.target_reached_at = nil
+      # action.army.mode = 0
+#       
+      # if !action.army.save
+        # runloop.say "Army #{action.army_id} could not be moved to new location. Save did fail.", Logger::ERROR
+        # return
+      # end
+      # runloop.say "Movement completed, cleaning up and destroying event."      
+      # started_battles = action.army.check_for_battle_at(target_location)
+      # runloop.say "Started #{started_battles} battles automatically."      
+      # action.destroy
+      # event.destroy
+      # runloop.say "Movement handler completed."
+    # end
+  end
+end
+
