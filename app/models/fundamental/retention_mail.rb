5107b6b09ee4c2a89acd04c43239a3c6f0a12a93
Patrick <p@trick-fox.de>
2012-11-23 17:53:07 +0100

    temporarily reqard toads instead of credits to non paying user due to a issue with the bytro shop

--- app/models/fundamental/retention_mail.rb
+++ app/models/fundamental/retention_mail.rb
@@ -10,39 +10,43 @@ class Fundamental::RetentionMail < ActiveRecord::Base
   before_save  :add_identifier
    
   def redeem_credit_reward
-    booking_amount = -self.credit_reward    # for adding credits the amount has to be negative
-    return false if booking_amount >= 0
-    
-    # lokale transaction erzeugen
-    shop_transaction = Shop::Transaction.create({
-      character: character,
-      offer: 'redeemed retention mail reward',
-      state: Shop::Transaction::STATE_CREATED
-    })
-    
-    # transaction zum payment provider schicken
-    virtual_bank_transaction = {
-      customer_identifier: character.identifier,
-      credit_amount_booked: booking_amount,
-      booking_type: booking_amount > 0 ? Shop::Transaction::TYPE_DEBIT : Shop::Transaction::TYPE_CREDIT,
-      transaction_id: shop_transaction.id,
-    }
-    
-    account_response = CreditShop::BytroShop.get_customer_account(character.identifier)
-    return false unless account_response[:response_code] == Shop::Transaction::API_RESPONSE_OK
-    credit_amount = account_response[:response_data][:amount]
-     
-    shop_transaction.credit_amount_before = credit_amount
-    shop_transaction.save
-    
-    transaction_response = CreditShop::BytroShop.post_virtual_bank_transaction(virtual_bank_transaction, character.identifier)
-    if transaction_response[:response_code] === Shop::Transaction::API_RESPONSE_OK
-      shop_transaction.credit_amount_after = transaction_response[:response_data][:amount]
-      shop_transaction.state = Shop::Transaction::STATE_CLOSED
-      shop_transaction.credit_amount_booked = booking_amount
+    if character.max_conversion_state == 'paying'
+      booking_amount = -self.credit_reward    # for adding credits the amount has to be negative
+      return false if booking_amount >= 0
+      
+      # lokale transaction erzeugen
+      shop_transaction = Shop::Transaction.create({
+        character: character,
+        offer: 'redeemed retention mail reward',
+        state: Shop::Transaction::STATE_CREATED
+      })
+      
+      # transaction zum payment provider schicken
+      virtual_bank_transaction = {
+        customer_identifier: character.identifier,
+        credit_amount_booked: booking_amount,
+        booking_type: booking_amount > 0 ? Shop::Transaction::TYPE_DEBIT : Shop::Transaction::TYPE_CREDIT,
+        transaction_id: shop_transaction.id,
+      }
+      
+      account_response = CreditShop::BytroShop.get_customer_account(character.identifier)
+      return false unless account_response[:response_code] == Shop::Transaction::API_RESPONSE_OK
+      credit_amount = account_response[:response_data][:amount]
+       
+      shop_transaction.credit_amount_before = credit_amount
       shop_transaction.save
+      
+      transaction_response = CreditShop::BytroShop.post_virtual_bank_transaction(virtual_bank_transaction, character.identifier)
+      if transaction_response[:response_code] === Shop::Transaction::API_RESPONSE_OK
+        shop_transaction.credit_amount_after = transaction_response[:response_data][:amount]
+        shop_transaction.state = Shop::Transaction::STATE_CLOSED
+        shop_transaction.credit_amount_booked = booking_amount
+        shop_transaction.save
+      else
+        false
+      end
     else
-      false
+      character.resource_pool.add_resources_transaction({3 => 10})
     end
   end
   
@@ -51,8 +55,8 @@ class Fundamental::RetentionMail < ActiveRecord::Base
       "In Deiner Siedlung geht's grad drunter und drüber"
     elsif mail_type == 'paused_too_long'
       "In Deiner Siedlung geht's grad drunter und drüber"
-    elsif mail_type == 'retention_getting_inactive'
-      "In Deiner Siedlung geht's grad drunter und drüber"
+    elsif mail_type == 'getting_inactive'
+      "#{character.name}, " + (character.max_conversion_state == 'paying' ? "#{credit_reward} 5D Platinum Credits" : "10 Goldkröten") + " - Hol dir deine Belohnung!"
     end
   end
   
@@ -69,13 +73,12 @@ class Fundamental::RetentionMail < ActiveRecord::Base
       "#{GAME_SERVER_CONFIG['base_url']}\n\n" +
       "Viel Spaß wünscht Dir\n\n" +
       "Dein Wack-A-Doo Team"
-    elsif mail_type == 'retention_getting_inactive'
-      "Hallo #{character.name},\n\n" +
-      "Deine Steine schimmeln, Dein Holz schlägt Wurzeln und Deinen Fellen fallen die Haare aus. Und der Dino-Nachwuchs macht auch was er will - kurz: in Deiner Siedlung geht's drunter und drüber. Log Dich jetzt ein und sorge für Ordnung:\n\n" +
-      "#{GAME_SERVER_CONFIG['base_url']}\n\n" +
-      "Als Belohnung warten #{credit_reward} 5D Platinum Credits im Shop auf Dich!\n\n" +
-      "Viel Spaß wünscht Dir\n\n" +
-      "Dein Wack-A-Doo Team"
+    elsif mail_type == 'getting_inactive'
+      "#{character.name}!\n\n" +
+      "Du hast Dein Können bewiesen und unsere Siedlung aufgebaut, aber wir Siedlungsbewohner haben schon lange nichts mehr von Dir gehört. Deine Gegner haben Dich schon abgeschrieben, aber ich glaube weiterhin an Dich!\n\n" + 
+      "Log Dich umgehend unter #{GAME_SERVER_CONFIG['base_url']} ein und zeig es ihnen! Als Unterstützung gebe ich Dir " + (character.max_conversion_state == 'paying' ? "#{credit_reward} 5D Platinum Credits" : "10 Goldkröten") + " aus meiner persönlichen Schatzkiste.\n\n" +
+      "Deine Siedlungsbewohner erwarten Dich bereits! Viel Erfolg!\n\n" +
+      "Der Chef\n\n"      
     end
   end
   
