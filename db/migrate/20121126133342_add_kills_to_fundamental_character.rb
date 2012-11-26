772a23f80e9ec11e132986a7097993d65a94fbca
salange <sascha@5dlab.com>
2012-11-26 14:57:42 +0100

    missing type in migration

--- db/migrate/20121126133342_add_kills_to_fundamental_character.rb
+++ db/migrate/20121126133342_add_kills_to_fundamental_character.rb
@@ -2,9 +2,9 @@ class AddKillsToFundamentalCharacter < ActiveRecord::Migration
   def change
     add_column :fundamental_characters,     :kills,      :integer, :default => 0, :null => false
     add_column :ranking_character_rankings, :kills,      :integer, :default => 0, :null => false
-    add_column :ranking_character_rankings, :kills_rank
+    add_column :ranking_character_rankings, :kills_rank, :integer
     add_column :ranking_alliance_rankings,  :kills,      :integer, :default => 0, :null => false
-    add_column :ranking_allinace_rankings,  :kills_rank
+    add_column :ranking_allinace_rankings,  :kills_rank, :integer
 
   end
 end
