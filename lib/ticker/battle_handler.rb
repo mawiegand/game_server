f0105bd2eb4fd34ea35c3d87f340fceebadfa7bd
Julian Schmid <jschmid@murmin.com>
2012-11-29 19:32:45 +0100

    removed old message factory

--- lib/ticker/battle_handler.rb
+++ lib/ticker/battle_handler.rb
@@ -1,13 +1,11 @@
 require 'awe_native_extensions'
 require 'ticker/battle_handler_awe_factory'
 require 'ticker/battle_handler_result_extractor'
-require 'ticker/battle_handler_message_factory'
 require 'ticker/battle_handler/battle_summary'
 
 class Ticker::BattleHandler
   
   include Ticker::BattleHandlerResultExtractor
-  include Ticker::BattleHandlerMessageFactory
   include Ticker::BattleHandlerAweFactory
   
   def runloop 
@@ -71,7 +69,6 @@ class Ticker::BattleHandler
         battle_summary = Ticker::BattleHandler::BattleSummary.new(battle)
         #send messages
         battle_summary.send_battle_report_messages()
-        #generate_messages_for_battle(awe_battle, battle)
 
         runloop.say "Determine winner and check takeover"
 
