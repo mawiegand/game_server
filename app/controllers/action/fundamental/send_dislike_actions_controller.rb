d3f53e99dc897d4418deef055ab2bd737f4f20b6
Marcel Wiegand <marcel@bluepanel.org>
2012-12-01 03:44:15 +0100

    first working version of senddislike

---
+++ app/controllers/action/fundamental/send_dislike_actions_controller.rb
@@ -0,0 +1,23 @@
+class Action::Fundamental::SendDislikeActionsController < ApplicationController
+  layout 'action'
+
+  before_filter :authenticate
+
+  # POST /action/military/attack_army_actions
+  # POST /action/military/attack_army_actions.json
+  def create
+
+    raise BadRequestError.new('no current character') if current_character.nil?
+    raise BadRequestError.new('missing parameter(s)') if params[:character].nil? || params[:character][:id].blank?
+
+	dislike = LikeSystem::Dislike.new
+	dislike.sender = current_character
+  dislike.receiver = Fundamental::Character.find(params[:character][:id])
+	dislike.save
+
+    respond_to do |format|
+      format.json { render json: {}, status: :ok }
+    end
+  end
+
+end
