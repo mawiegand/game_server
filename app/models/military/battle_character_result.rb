66915ffc7501768dfd321717c37734cc24fe9a7c
salange <sascha@5dlab.com>
2012-12-01 15:49:18 +0100

    belongs_to instead of has_on

--- app/models/military/battle_character_result.rb
+++ app/models/military/battle_character_result.rb
@@ -1,8 +1,8 @@
 class Military::BattleCharacterResult < ActiveRecord::Base
 
-  has_one  :battle,     :class_name => "Military::Battle",        :foreign_key => "battle_id",     :inverse_of => :character_results
-  has_one  :character,  :class_name => "Fundamental::Character",  :foreign_key => "character_id",  :inverse_of => :battle_results
-  has_one  :faction,    :class_name => "Military::BattleFaction", :foreign_key => "faction_id"
+  belongs_to  :battle,     :class_name => "Military::Battle",        :foreign_key => "battle_id",     :inverse_of => :character_results
+  belongs_to  :character,  :class_name => "Fundamental::Character",  :foreign_key => "character_id",  :inverse_of => :battle_results
+  belongs_to  :faction,    :class_name => "Military::BattleFaction", :foreign_key => "faction_id"
   # don't reference faction: when faction is deleted, faction_id would be set to nil
 
 end
