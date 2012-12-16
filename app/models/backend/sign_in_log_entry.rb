0ac20da219093a4f5170b07ba0801597d77da505
salange <sascha@5dlab.com>
2012-12-16 11:26:02 +0100

    manually updated sign ups counter

--- app/models/backend/sign_in_log_entry.rb
+++ app/models/backend/sign_in_log_entry.rb
@@ -2,13 +2,14 @@ require 'uri'
 
 class Backend::SignInLogEntry < ActiveRecord::Base
   
-  belongs_to :character,     :class_name => "Fundamental::Character", :foreign_key => "character_id", :inverse_of => :sign_ins
+  belongs_to :character,     :class_name => "Fundamental::Character", :foreign_key => "character_id",    :inverse_of => :sign_ins
   belongs_to :partner_site,  :class_name => "Backend::PartnerSite",   :foreign_key => "partner_site_id", :inverse_of => :sign_ins
-  belongs_to :referer_site,  :class_name => "Backend::PartnerSite",   :foreign_key => "partner_site_id", :inverse_of => :sign_ups, :conditions => ["sign_up=?", true]
   
   before_save :determine_referer_from_url
   before_save :determine_tracking_params_from_request_url
   before_save :add_partner_site
+  
+  after_save  :update_sign_ups_count
   
   scope :signups,         where(sign_up: true)
   scope :refered_by_host, lambda { |host| where("referer LIKE '%#{host}%'") }
@@ -38,4 +39,15 @@ class Backend::SignInLogEntry < ActiveRecord::Base
       self.partner_site = Backend::PartnerSite.find_by_referer(self.referer) if self.partner_site.nil?
     end  
   
+    # this updates the count manually. we cannot use a counter cache, because rails' implementation
+    # of reset_counters presently is wrong; it fails in case there are two belongs_to associations
+    # with the same foreign key (which is wrongly used to select the inverse of the sign_ups has 
+    # many association). :-(
+    def update_sign_ups_count
+      if self.sign_up? && self.partner_site_id_changed? && !self.partner_site.nil?
+        self.partner_site.sign_ups_count = (self.partner_site.sign_ups_count || 0) + 1
+        self.partner_site.save
+      end
+    end
+        
 end
