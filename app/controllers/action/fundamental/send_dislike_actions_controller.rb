e82a8cd3e0afb3cef805c4826719900789a87b83
Marcel Wiegand <marcel@bluepanel.org>
2012-12-21 14:32:39 +0100

    charcter can only be liked/disliked ones a day by the same player

--- app/controllers/action/fundamental/send_dislike_actions_controller.rb
+++ app/controllers/action/fundamental/send_dislike_actions_controller.rb
@@ -10,10 +10,16 @@ class Action::Fundamental::SendDislikeActionsController < ApplicationController
     raise BadRequestError.new('no current character') if current_character.nil?
     raise BadRequestError.new('missing parameter(s)') if params[:character].nil? || params[:character][:id].blank?
     
-    dislike = LikeSystem::Dislike.new
-    dislike.sender = current_character
-    dislike.receiver = Fundamental::Character.find(params[:character][:id])
-    dislike.save
+    receiver = Fundamental::Character.find(params[:character][:id])
+    old_dislikes = LikeSystem::Dislike.where('sender_id = ? and receiver_id = ? and created_at > ?',
+                               current_character, receiver, 1.day.ago).all
+                             
+    if(old_dislikes.empty?)
+      dislike = LikeSystem::Dislike.new(:sender => current_character, :receiver => receiver)
+      dislike.save
+    else
+      raise BadRequestError.new('Allready sent dislike!')
+    end
 
     respond_to do |format|
       format.json { render json: {}, status: :ok }
--- app/controllers/action/fundamental/send_like_actions_controller.rb
+++ app/controllers/action/fundamental/send_like_actions_controller.rb
@@ -10,10 +10,16 @@ class Action::Fundamental::SendLikeActionsController < ApplicationController
     raise BadRequestError.new('no current character') if current_character.nil?
     raise BadRequestError.new('missing parameter(s)') if params[:character].nil? || params[:character][:id].blank?
 
-    like = LikeSystem::Like.new
-    like.sender = current_character
-    like.receiver = Fundamental::Character.find(params[:character][:id])
-    like.save
+    receiver = Fundamental::Character.find(params[:character][:id])
+    old_likes = LikeSystem::Like.where('sender_id = ? and receiver_id = ? and created_at > ?',
+                               current_character, receiver, 1.day.ago).all
+                             
+    if(old_likes.empty?)
+      like = LikeSystem::Like.new(:sender => current_character, :receiver => receiver)
+      like.save
+    else
+      raise BadRequestError.new('Allready sent like!')
+    end
 
     respond_to do |format|
       format.json { render json: {}, status: :ok }
