059d57131d3c2400628bddb916578a8968228504
Patrick <p@trick-fox.de>
2012-12-29 02:27:23 +0100

    changed required region ratio from simple value to a formula depending on round age

--- app/models/fundamental/round_info.rb
+++ app/models/fundamental/round_info.rb
@@ -7,4 +7,9 @@ class Fundamental::RoundInfo < ActiveRecord::Base
   def victory_gained?
     !self.victory_gained_at.nil?
   end
+  
+  # return age of round in full days (integer)
+  def age
+    ((Time.now - self.started_at)/(3600*24)).to_i
+  end
 end
