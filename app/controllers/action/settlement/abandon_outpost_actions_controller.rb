542e4533a3e874157c22a7c8b6e6a9148bfabcc7
Patrick <p@trick-fox.de>
2013-01-02 10:58:29 +0100

    reduce haunt if level is greater than 1

--- app/controllers/action/settlement/abandon_outpost_actions_controller.rb
+++ app/controllers/action/settlement/abandon_outpost_actions_controller.rb
@@ -4,26 +4,26 @@ class Action::Settlement::AbandonOutpostActionsController < ApplicationControlle
 
   def create
     
-	raise BadRequestError.new('no current character') if current_character.nil?
-	raise BadRequest.new('missing parameter(s)') if params[:abandon_outpost_action].nil? || params[:abandon_outpost_action][:settlement_id].blank?
-	
-	neandertaler = Fundamental::Character.find_by_id(1)
+  	raise BadRequestError.new('no current character') if current_character.nil?
+  	raise BadRequest.new('missing parameter(s)') if params[:abandon_outpost_action].nil? || params[:abandon_outpost_action][:settlement_id].blank?
+  	
+  	neandertaler = Fundamental::Character.find_by_id(1)
     settlement = Settlement::Settlement.find_by_id(params[:abandon_outpost_action][:settlement_id])
-
-	raise ForbiddenError.new('tried to abandon a foreign settlement.')   unless  settlement.owner == current_character
-	raise ForbiddenError.new('tried to abandon a settlement with forbidden typ.')   unless  settlement.type_id == Settlement::Settlement::TYPE_OUTPOST
-	
-	settlement.slots.each do |slot|
-	  if slot.slot_num == 2
-	    slot.destroy_building
-	  else
-	    if slot.slot_num > 2
-		  slot.donwgrade_building
-		end
-	  end
-	end
-	
-	settlement.new_owner_transaction(neandertaler) #Übergabe an Neandertaler 
+  
+  	raise ForbiddenError.new('tried to abandon a foreign settlement.')   unless  settlement.owner == current_character
+  	raise ForbiddenError.new('tried to abandon a settlement with forbidden typ.')   unless  settlement.type_id == Settlement::Settlement::TYPE_OUTPOST
+  	
+  	settlement.slots.each do |slot|
+  	  if slot.slot_num == 1 && !slot.level.nil? && slot.level > 1
+        slot.donwgrade_building
+  	  elsif slot.slot_num == 2
+  	    slot.destroy_building
+  	  elsif slot.slot_num > 2
+        slot.donwgrade_building
+  	  end
+  	end
+  	
+  	settlement.new_owner_transaction(neandertaler) #Uebergabe an Neandertaler 
     
     respond_to do |format|
       format.json { render json: {}, status: :ok }
