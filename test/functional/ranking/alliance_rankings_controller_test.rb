f496d91b6898c683fc0fd388512e89b5069aa675
Patrick <p@trick-fox.de>
2013-01-06 02:18:03 +0100

    repaired tests

--- test/functional/ranking/alliance_rankings_controller_test.rb
+++ test/functional/ranking/alliance_rankings_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Ranking::AllianceRankingsControllerTest < ActionController::TestCase
   setup do
     @ranking_alliance_ranking = ranking_alliance_rankings(:one)
+    @controller.current_backend_user = backend_users(:staff)  # this is a quick hack to make the scaffolded tests pass. Must be moved to individual tests later.
   end
 
   test "should get index" do
--- test/functional/ranking/fortress_rankings_controller_test.rb
+++ test/functional/ranking/fortress_rankings_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class Ranking::FortressRankingsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ranking_fortress_ranking = settlement_settlements(:one)
+    @controller.current_backend_user = backend_users(:staff)  # this is a quick hack to make the scaffolded tests pass. Must be moved to individual tests later.
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
 end
