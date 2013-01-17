50da536290d2224e3eeba6bef79b28ff436bf6e4
Marcel Wiegand <marcel@bluepanel.org>
2013-01-17 01:54:35 +0100

    refactoring of history_caching

--- app/controllers/fundamental/history_events_controller.rb
+++ app/controllers/fundamental/history_events_controller.rb
@@ -7,26 +7,11 @@ class Fundamental::HistoryEventsController < ApplicationController
 
   def index
     @fundamental_character = Fundamental::Character.find(params[:character_id])
-    
-#    identity_provider_access = IdentityProvider::Access.new({
-#      identity_provider_base_url: GAME_SERVER_CONFIG['identity_provider_base_url'],
-#      game_identifier:            GAME_SERVER_CONFIG['game_identifier'],
-#      scopes:                     ['5dentity'],
-#    })
-#    response = identity_provider_access.fetch_history_events(@fundamental_character.identifier)
-#    
-#    if response.code == 200
-#      @fundamental_history_events = response.parsed_response.nil? ? nil : response.parsed_response
-      @fundamental_history_events = Fundamental::HistoryEvent.history_events_for_character_id(params[:character_id])
-      @fundamental_history_events.each do |event|
-        event['character_id'] = params[:character_id]
-      end
-#    elsif response.code == 404
-#      @not_found = true
-#    else
-#      @error_code = response.code
-#      @error = response.body
-#    end
+	
+    @fundamental_history_events = Fundamental::HistoryEvent.history_events_for_character_id(params[:character_id])
+    @fundamental_history_events.each do |event|
+      event['character_id'] = params[:character_id]
+    end
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/fundamental/history_event.rb
+++ app/models/fundamental/history_event.rb
@@ -22,23 +22,15 @@ class Fundamental::HistoryEvent < ActiveRecord::Base
     })
     response = identity_provider_access.fetch_history_events(@fundamental_character.identifier)
     Rails.logger.debug "HISTORY #{response.code}"
-#    url = GAME_SERVER_CONFIG['identity_provider_base_url']+"/resource/histories"
-#    
-#    header = Hash.new
-#    header['Accept'] = 'application/json'
-#    header['If-Modified-Since'] = Fundamental::HistoryEvent.maximum(:updated_at).httpdate unless Fundamental::HistoryEvent.first.nil?
-#    
-#    response = HTTParty.get(url, :headers => header)
+    
     httpstatus = response.code
     
     if httpstatus == 200
       response.parsed_response.each do |item|
         if Fundamental::HistoryEvent.exists?(item['id'])
           event = Fundamental::HistoryEvent.find(item['id'])
-		  Rails.logger.debug "FOUND HISTORY"
 		else
           event = Fundamental::HistoryEvent.new
-		  Rails.logger.debug "NOT FOUND HISTORY"
         end
         
         if event != nil
