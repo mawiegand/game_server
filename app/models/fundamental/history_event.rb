e57f3d57b943db07e0ff195a3fd16a5393578068
Marcel Wiegand <marcel@bluepanel.org>
2013-01-18 22:35:55 +0100

    update HistoryEvents only if update time intervall is reached Intervall is defined in GAME_SERVER_CONFIG['history_update_intervall']

--- app/models/fundamental/history_event.rb
+++ app/models/fundamental/history_event.rb
@@ -13,39 +13,38 @@ class Fundamental::HistoryEvent < ActiveRecord::Base
     Fundamental::HistoryEvent.find(:all, :conditions => { :character_id => character_id})
   end
   
-  def self.update_if_necessary(character_id)
-    @fundamental_character = Fundamental::Character.find(character_id)
-    identity_provider_access = IdentityProvider::Access.new({
-      identity_provider_base_url: GAME_SERVER_CONFIG['identity_provider_base_url'],
-      game_identifier:            GAME_SERVER_CONFIG['game_identifier'],
-      scopes:                     ['5dentity'],
-    })
-    response = identity_provider_access.fetch_history_events(@fundamental_character.identifier)
-    Rails.logger.debug "HISTORY #{response.code}"
-    
-    httpstatus = response.code
-    
-    if httpstatus == 200
-      response.parsed_response.each do |item|
-        if Fundamental::HistoryEvent.exists?(item['id'])
-          event = Fundamental::HistoryEvent.find(item['id'])
-		else
-          event = Fundamental::HistoryEvent.new
-        end
-        
-        if event != nil
-          event.id = item['id']
-          event.created_at = item['created_at']
-          event.updated_at = item['updated_at']
-          event.data = item['data']
-          event.localized_description = item['localized_description']
-          event.character = @fundamental_character
+  def self.update_if_necessary(character_id) 
+    # check if update intervall is reached
+    if GAME_SERVER_CONFIG['history_update_intervall'] >= Fundamental::HistoryEvent.where(:character_id => character_id).maximum(:updated_at)
+      @fundamental_character = Fundamental::Character.find(character_id)
+      
+      identity_provider_access = IdentityProvider::Access.new({
+        identity_provider_base_url: GAME_SERVER_CONFIG['identity_provider_base_url'],
+        game_identifier:            GAME_SERVER_CONFIG['game_identifier'],
+        scopes:                     ['5dentity'],
+      })
+      response = identity_provider_access.fetch_history_events(@fundamental_character.identifier)
+      
+      if response.code == 200
+        response.parsed_response.each do |item|
+          if Fundamental::HistoryEvent.exists?(item['id'])
+            event = Fundamental::HistoryEvent.find(item['id'])
+          else
+            event = Fundamental::HistoryEvent.new
+          end
           
-          event.save
+          if event != nil
+            event.id = item['id']
+            event.data = item['data']
+            event.localized_description = item['localized_description']
+            event.character = @fundamental_character
+            
+            event.save
+          end
         end
+      else
+        Rails.logger.debug "An error occured while fetching history events! Http-Status: #{httpstatus}"
       end
-    else
-      Rails.logger.debug "An error occured while fetching history events! Http-Status: #{httpstatus}"
     end
   end
 end
