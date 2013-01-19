137ae34ea2c4dc7a348b20bfe49b6d80726a48c3
Patrick <p@trick-fox.de>
2013-01-19 22:59:25 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116134728) do
+ActiveRecord::Schema.define(:version => 20130117102123) do
 
   create_table "action_military_attack_army_actions", :force => true do |t|
     t.integer  "attacker_id"
@@ -1263,13 +1263,12 @@ ActiveRecord::Schema.define(:version => 20130116134728) do
     t.decimal  "exp_production_rate",                      :default => 0.0,   :null => false
     t.decimal  "exp_building_production_rate",             :default => 0.0,   :null => false
     t.datetime "production_updated_at"
-    t.integer  "victories",                                :default => 0,     :null => false
-    t.integer  "defeats",                                  :default => 0,     :null => false
     t.integer  "send_likes_count",                         :default => 0
     t.integer  "received_likes_count",                     :default => 0
     t.integer  "send_dislikes_count",                      :default => 0
     t.integer  "received_dislikes_count",                  :default => 0
     t.string   "same_ip"
+    t.boolean  "deleted_from_game",                        :default => false
   end
 
   create_table "fundamental_guilds", :force => true do |t|
