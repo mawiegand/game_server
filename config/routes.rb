29bcbe436719df836f9027829527093558f0e029
Patrick <p@trick-fox.de>
2013-02-27 20:34:49 +0100

    added effect dialog to resource production overview

--- config/routes.rb
+++ config/routes.rb
@@ -50,11 +50,14 @@ GameServer::Application.routes.draw do
           resources :history_events,                           :only => [ :index ]
         end
         
-        resources :resource_pools 
+        resources :resource_pools do
+          resources :resource_effects, :module => "effect"
+        end
         
         resources :alliances do
           resources :characters
           resources :alliance_shouts
+          resources :alliance_resource_effects, :module => "effect"
         end
         
         resources :victory_progresses 
@@ -273,61 +276,4 @@ GameServer::Application.routes.draw do
       match '/signout', :to => 'auth/sessions#destroy'
     end    
   end
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
