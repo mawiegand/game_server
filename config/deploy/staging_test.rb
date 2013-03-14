977f6042134e0f630971099ada7a2b3adfa0d7c4
Patrick <p@trick-fox.de>
2013-03-14 18:40:28 +0100

    changed subdomain

--- config/deploy/staging_test.rb
+++ config/deploy/staging_test.rb
@@ -1,6 +1,6 @@
-role :web,       'test2.wack-a-doo.de'                          # Your HTTP server, Apache/etc
-role :app,       'test2.wack-a-doo.de'                          # This may be the same as your `Web` server
-role :db,        'test2.wack-a-doo.de', :primary => true        # This is where Rails migrations will run
+role :web,       'staging-test.wack-a-doo.de'                          # Your HTTP server, Apache/etc
+role :app,       'staging-test.wack-a-doo.de'                          # This may be the same as your `Web` server
+role :db,        'staging-test.wack-a-doo.de', :primary => true        # This is where Rails migrations will run
 
 set :port,       22
 
