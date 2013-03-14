a07ea3f617107618c434a53b7cb4f13fe5540a26
Christian Wansart <christian@bluepanel.org>
2013-03-14 23:31:18 +0100

    fixed trade resource bug

--- app/controllers/action/fundamental/trade_resources_actions_controller.rb
+++ app/controllers/action/fundamental/trade_resources_actions_controller.rb
@@ -2,11 +2,13 @@ class Action::Fundamental::TradeResourcesActionsController < ApplicationControll
   layout 'action'
   before_filter :authenticate
 
-  EXCHANGE_FEE = 3 # 3 gold toads, when changed, please change in html_client as well
+  EXCHANGE_FEE  = 3 # 3 gold toads, when changed, please change in html_client as well
+  MAX_OVERFLOW  = 1.05 #percentage
+  MAX_UNDERFLOW = 0.98
 
   def create
-    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) > 1.05
-    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) < 0.98
+    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) > MAX_OVERFLOW
+    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) < MAX_UNDERFLOW
     raise ForbiddenError.new('negative value(s)') if (params[:resource_stone].to_f < 0.0) || (params[:resource_wood].to_f < 0.0) | (params[:resource_fur].to_f < 0.0)
 
     pool = current_character.resource_pool
@@ -17,12 +19,49 @@ class Action::Fundamental::TradeResourcesActionsController < ApplicationControll
     raise ForbiddenError.new('not enough cash') if pool.resource_cash_amount < EXCHANGE_FEE
 
     # check if user has enough capacity
-    raise ForbiddenError.new('not enough capacity') if (params[:resource_stone].to_f*sum) > pool.resource_stone_capacity || (params[:resource_wood].to_f*sum) > pool.resource_wood_capacity || (params[:resource_fur].to_f*sum) > pool.resource_fur_capacity
+    # TODO: need to check those values a bit more. due to the
+    #       decimal places and rounding on the client side,
+    #       the new values are higher that they should be.
+    new_stone_value = sum*params[:resource_stone].to_f
+    new_wood_value  = sum*params[:resource_wood].to_f
+    new_fur_value   = sum*params[:resource_fur].to_f
+    max_inaccuracy  = (MAX_OVERFLOW*100 - 100).to_f
+
+    raise ForbiddenError.new("#{sum} #{params[:resource_stone].to_f} #{params[:resource_wood].to_f} #{params[:resource_fur].to_f}")
+
+    # due to the rounding in the client, it may happen that the capacity is exceeded, thus
+    # we're checking if that happens and if it does wihting our inaccuracy limitation, we assign
+    # the capacity to the value, ensuring that the user gets what he/she assigned.
+    #
+    # TODO: refactor here. 3 times the same step only with 1 difference.
+    if new_stone_value > pool.resource_stone_capacity
+      if (new_stone_value-max_inaccuracy) > pool.resource_stone_capacity
+        raise ForbiddenError.new('stone capacity exceeded')
+      else
+        new_stone_value = pool.resource_stone_capacity
+      end
+    end
+
+    if new_wood_value > pool.resource_wood_capacity
+      if new_wood_value > pool.resource_wood_capacity*MAX_OVERFLOW
+        raise ForbiddenError.new("wood capacity exceeded")
+      else
+        new_wood_value = pool.resource_wood_capacity
+      end
+    end
+
+    if new_fur_value > pool.resource_fur_capacity
+      if (new_fur_value-max_inaccuracy) > pool.resource_fur_capacity
+        raise ForbiddenError.new('fur capacity exceeded')
+      else
+        new_fur_value = pool.resource_fur_capacity
+      end
+    end
 
     # change resources
-    pool.resource_stone_amount = sum*params[:resource_stone].to_f
-    pool.resource_wood_amount  = sum*params[:resource_wood].to_f
-    pool.resource_fur_amount   = sum*params[:resource_fur].to_f
+    pool.resource_stone_amount = new_stone_value.to_f
+    pool.resource_wood_amount  = new_wood_value.to_f
+    pool.resource_fur_amount   = new_fur_value.to_f
     pool.resource_cash_amount -= EXCHANGE_FEE
 
     # save new values
