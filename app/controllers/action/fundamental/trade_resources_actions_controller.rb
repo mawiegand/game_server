9a40f7645eee9c432d81870c2ad4d8ec6bad1ae1
Christian Wansart <christian@bluepanel.org>
2013-03-21 17:15:01 +0100

    re-wrote trade resources action to work with the re-designed dialog

--- app/controllers/action/fundamental/trade_resources_actions_controller.rb
+++ app/controllers/action/fundamental/trade_resources_actions_controller.rb
@@ -3,65 +3,55 @@ class Action::Fundamental::TradeResourcesActionsController < ApplicationControll
   before_filter :authenticate
 
   EXCHANGE_FEE  = 3 # 3 gold toads, when changed, please change in html_client as well
-  MAX_OVERFLOW  = 1.05 #percentage
-  MAX_UNDERFLOW = 0.98
 
   def create
-    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) > MAX_OVERFLOW
-    raise BadRequestError.new('bad parameter(s)') if (params[:resource_stone].to_f + params[:resource_wood].to_f + params[:resource_fur].to_f) < MAX_UNDERFLOW
-    raise ForbiddenError.new('negative value(s)') if (params[:resource_stone].to_f < 0.0) || (params[:resource_wood].to_f < 0.0) | (params[:resource_fur].to_f < 0.0)
+    pStone = params[:resource_stone].to_f
+    pWood  = params[:resource_wood].to_f
+    pFur   = params[:resource_fur].to_f
+    
+    raise ForbiddenError.new('negative value(s)') if pStone < 0.0 || pWood < 0.0 || pFur < 0.0
+    raise ForbiddenError.new('is this user dumb?! Why would anybody want to get 0 ressources?!') if pStone == 0.0 && pWood == 0.0 && pFur == 0.0
 
     Fundamental::ResourcePool.transaction do
       pool = current_character.resource_pool.lock!
       pool.update_resource_amount_atomically
       sum = pool.resource_stone_amount + pool.resource_wood_amount + pool.resource_fur_amount
+      remaining = sum - (pStone + pWood + pFur)
 
       # check if user has enough cash
       raise ForbiddenError.new('not enough cash') if pool.resource_cash_amount < EXCHANGE_FEE
 
-      # check if user has enough capacity
-      # TODO: need to check those values a bit more. due to the
-      #       decimal places and rounding on the client side,
-      #       the new values are higher that they should be.
-      new_stone_value = sum*params[:resource_stone].to_f
-      new_wood_value  = sum*params[:resource_wood].to_f
-      new_fur_value   = sum*params[:resource_fur].to_f
+      # check if transmitted sum is bigger than the sum of the resource pool
+      raise ForbiddenError.new('sum is greater than sum from respool') if remaining < 0 
+      
+      # check if each resource is greater than it's maximum capacity
+      raise ForbiddenError.new('stone is greater than it\'s capacity') if pStone > pool.resource_stone_capacity
+      raise ForbiddenError.new('wood is greater than it\'s capacity')  if pWood > pool.resource_wood_capacity
+      raise ForbiddenError.new('fur is greater than it\'s capacity')   if pFur > pool.resource_fur_capacity
 
-      #raise ForbiddenError.new("#{sum} #{params[:resource_stone].to_f} #{params[:resource_wood].to_f} #{params[:resource_fur].to_f}")
-
-      # due to the rounding in the client, it may happen that the capacity is exceeded, thus
-      # we're checking if that happens and if it does wihting our inaccuracy limitation, we assign
-      # the capacity to the value, ensuring that the user gets what he/she assigned.
-      #
-      # TODO: refactor here. 3 times the same step only with 1 difference.
-      if new_stone_value > pool.resource_stone_capacity
-        if new_stone_value > pool.resource_stone_capacity*MAX_OVERFLOW
-          raise ForbiddenError.new('stone capacity exceeded')
-        else
-          new_stone_value = pool.resource_stone_capacity
-        end
+      # dividing remaining by 3 will force us to check if the capacity is reached and dividing another time.
+      # Doing this will increase complexity of the code too much that I decided to go the simple way.
+      remainEach = remaining/3
+      remaining -= remaining.to_i
+      remainEach.to_i.times do
+        pStone += 1.0 if pStone < pool.resource_stone_capacity 
+        pWood  += 1.0 if pWood  < pool.resource_wood_capacity
+        pFur   += 1.0 if pFur   < pool.resource_fur_capacity
       end
 
-      if new_wood_value > pool.resource_wood_capacity
-        if new_wood_value > pool.resource_wood_capacity*MAX_OVERFLOW
-          raise ForbiddenError.new("wood capacity exceeded")
-        else
-          new_wood_value = pool.resource_wood_capacity
-        end
+      # assigning the rest
+      if pStone+remaining < pool.resource_stone_capacity
+        pStone += remaining
+      elsif pWood+remaining < pool.resource_wood_capacity
+        pWood += remaining
+      else
+        pFur += remaining
       end
 
-      if new_fur_value > pool.resource_fur_capacity
-        if new_fur_value > pool.resource_fur_capacity*MAX_OVERFLOW
-          raise ForbiddenError.new('fur capacity exceeded')
-        else
-          new_fur_value = pool.resource_fur_capacity
-        end
-      end
-
-      # change resources
-      pool.resource_stone_amount = new_stone_value.to_f
-      pool.resource_wood_amount  = new_wood_value.to_f
-      pool.resource_fur_amount   = new_fur_value.to_f
+      # assign new resource values
+      pool.resource_stone_amount = pStone 
+      pool.resource_wood_amount  = pWood
+      pool.resource_fur_amount   = pFur
       pool.resource_cash_amount -= EXCHANGE_FEE
 
       # save new values
