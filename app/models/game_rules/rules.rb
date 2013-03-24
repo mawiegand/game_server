0d71815aece0fe4ea555a5ca6bf486aa2b465d81
Patrick <p@trick-fox.de>
2013-03-24 17:51:52 +0100

    raised build numbers

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.0.3
+# Version: 0.0.4
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -113,7 +113,7 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 0,
-                    :build => 3,
+                    :build => 4,
       },
       :battle => {
         :calculation => {
