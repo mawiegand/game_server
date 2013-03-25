206df9a71003e0598d5119250aa28f2ba91a4e3b
hajorunne <hajo@5dlab.com>
2013-03-25 14:36:56 +0100

    Start Rank without settlement point & tutorial chief hut reward increased

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -6586,7 +6586,7 @@ Hinter der Häuptlingshütte ist ein kleiner Lagerplatz, auf dem Rohstoffe zwisc
         {              #  0
           :id          => 0, 
           :exp         => 0,
-          :settlement_points   => 1,
+          :settlement_points   => 0,
           :minimum_sacred_rank => 0,
           :name        => {
             
