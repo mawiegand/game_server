182577625d283d661d511e6c9305b8bcdf188c85
Patrick <p@trick-fox.de>
2013-03-25 11:22:22 +0100

    prepared for backup server

--- config/deploy/round2.rb
+++ config/deploy/round2.rb
@@ -2,7 +2,7 @@ role :web, "backup-round2.wack-a-doo.de"                          # Your HTTP se
 role :app, "backup-round2.wack-a-doo.de"                          # This may be the same as your `Web` server
 role :db,  "backup-round2.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
 
-set :rails_env, 'production'
+set :rails_env, 'round2'
 set :port, 5775                                          # until 5775 is working!
 
 set :branch,    "backup-round2"
