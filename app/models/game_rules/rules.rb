062cc8e1e2e11181fba29e1b19256784d6486cad
hajorunne <hajo@5dlab.com>
2013-03-26 13:30:41 +0100

    make

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.1.8
+# Version: 0.1.9
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -114,7 +114,7 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 1,
-                    :build => 8,
+                    :build => 9,
       },
       :battle => {
         :calculation => {
