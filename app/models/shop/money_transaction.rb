01f5b1c5eafdef8f6a4344963ca7fc0462cc3212
salange <sascha@5dlab.com>
2013-03-27 19:03:31 +0100

    aggregate money transactions only from start of round and do not consider charge backs

--- app/models/shop/money_transaction.rb
+++ app/models/shop/money_transaction.rb
@@ -1,5 +1,9 @@
 class Shop::MoneyTransaction < ActiveRecord::Base
   
-  scope :offer, lambda { |id| where(offer_id: id) }
+  scope :offer,          lambda { |id|   where(offer_id: id) }
+  scope :since_date,     lambda { |date| where('updatetstamp >= ?', date) }
+  scope :completed,      where("transaction_state LIKE 'completed'")
+  scope :no_charge_back, where('chargeback < 0.5')
+  scope :charge_back,    where('chargeback > 0.5')
   
 end
