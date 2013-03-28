f65aef5c3f9cbce6dc55156d7c2d61d4d6a7ac89
salange <sascha@5dlab.com>
2013-03-28 10:23:10 +0100

    only one training cave

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.1.16
+# Version: 0.1.17
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -114,7 +114,7 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 1,
-                    :build => 16,
+                    :build => 17,
       },
       :battle => {
         :calculation => {
@@ -3250,6 +3250,17 @@ class GameRules::Rules
             {
               :symbolic_id => 'building_haunt',
               :id => 21,
+              :type => 'building',
+
+              :min_level => 0,
+
+              :max_level => 0,
+
+            },
+
+            {
+              :symbolic_id => 'building_training_cave',
+              :id => 8,
               :type => 'building',
 
               :min_level => 0,
