585e300cd6405d51fd2e454df0d9d9bdad77955e
Patrick <p@trick-fox.de>
2013-04-03 17:03:15 +0200

    fake model for ios

--- app/controllers/shop/accounts_controller.rb
+++ app/controllers/shop/accounts_controller.rb
@@ -8,7 +8,12 @@ class Shop::AccountsController < ApplicationController
     account_response = CreditShop::BytroShop.get_customer_account(current_character.identifier)
     raise BadRequestError.new("Could not connect to Shop") if (account_response[:response_code] === Shop::Transaction::API_RESPONSE_ERROR)
     raise NotFoundError.new("User not found in Shop") if (account_response[:response_code] === Shop::Transaction::API_RESPONSE_USER_NOT_FOUND)
-    @shop_account = {shop_account: {credit_amount: account_response[:response_data][:amount]}}
+    @shop_account = {
+      id:            current_character.id,
+      created_at:    Time.now,
+      updated_at:    Time.now,
+      credit_amount: account_response[:response_data][:amount]
+    }
     
     respond_to do |format|
       format.html # show.html.erb
