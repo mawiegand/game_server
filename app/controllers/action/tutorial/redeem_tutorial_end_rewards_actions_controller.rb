03393b62be60eade10a6efa86c76e8c70ceb64ef
salange <sascha@5dlab.com>
2013-04-12 12:04:36 +0200

    adapted check in action for new field in tutorial state

--- app/controllers/action/tutorial/redeem_tutorial_end_rewards_actions_controller.rb
+++ app/controllers/action/tutorial/redeem_tutorial_end_rewards_actions_controller.rb
@@ -6,7 +6,7 @@ class Action::Tutorial::RedeemTutorialEndRewardsActionsController < ApplicationC
   def create
     tutorial_state = current_character.tutorial_state
     raise BadRequestError.new('tutorial state not found') if tutorial_state.nil?
-    raise ForbiddenError.new('tutorial end quest not completed yet') unless tutorial_state.completed_tutorial_end_quest?
+    raise ForbiddenError.new('tutorial end quest not completed yet') unless tutorial_state.completed_tutorial_end_quest? || tutorial_state.tutorial_finished?
     raise ForbiddenError.new('tutorial end rewards already rewarded') if tutorial_state.tutorial_completed
 
     current_character.redeem_tutorial_end_rewards
