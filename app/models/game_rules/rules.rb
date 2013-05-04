f84914d48e8eebee07f2c3408ed2b98fdf8c9f84
Marcel Wiegand <marcel@bluepanel.org>
2013-05-04 11:18:50 +0200

    changed formular of alliance_extension

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.2.0
+# Version: 0.2.1
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -114,7 +114,7 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 2,
-                    :build => 0,
+                    :build => 1,
       },
       :battle => {
         :calculation => {
@@ -3041,7 +3041,7 @@ class GameRules::Rules
 
             :unlock_alliance_creation => 2,
 
-            :alliance_size_bonus => "MAX(LEVEL-10,0)",
+            :alliance_size_bonus => "MAX(LEVEL-5,0)",
 
           },
 
