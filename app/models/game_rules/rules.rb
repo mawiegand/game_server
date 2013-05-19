7ff06d23e1c662605bf397a7954466be5e66439a
salange <sascha@5dlab.com>
2013-05-19 14:19:02 +0200

    corrected costs of alliance hall (level 0 was free)

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.2.2
+# Version: 0.3.0
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -113,8 +113,8 @@ class GameRules::Rules
     @the_rules ||= GameRules::Rules.new(
   
       :version => { :major => 0,
-                    :minor => 2,
-                    :build => 2,
+                    :minor => 3,
+                    :build => 0,
       },
       :battle => {
         :calculation => {
@@ -5382,7 +5382,7 @@ Hinter der Häuptlingshütte ist ein kleiner Lagerplatz, auf dem Rohstoffe zwisc
 
           :hidden      => 0,
 
-	        :population  => "LESS(LEVEL,11)*150+LEVEL*50",
+	        :population  => "LESS(LEVEL,11)*200",
   
           :buyable     => true,
           :demolishable=> false,
@@ -5438,9 +5438,9 @@ Hinter der Häuptlingshütte ist ein kleiner Lagerplatz, auf dem Rohstoffe zwisc
           ],          
 
           :costs      => {
-            0 => 'LESS(LEVEL,11)*25000*POW((LEVEL-1),1.25)',
-            1 => 'LESS(LEVEL,11)*25000*POW((LEVEL-1),1.25)',
-            2 => 'LESS(LEVEL,11)*25000*POW((LEVEL-1),1.25)',
+            0 => 'LESS(LEVEL,11)*25000*POW(LEVEL,1.25)',
+            1 => 'LESS(LEVEL,11)*25000*POW(LEVEL,1.25)',
+            2 => 'LESS(LEVEL,11)*25000*POW(LEVEL,1.25)',
             
           },
 
