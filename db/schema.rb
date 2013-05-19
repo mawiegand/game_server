01c20a45abbf37aa3d55b187733c861c48b02e94
salange <sascha@5dlab.com>
2013-05-19 18:23:19 +0200

    ran new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515112415) do
+ActiveRecord::Schema.define(:version => 20130516164041) do
 
   create_table "action_military_attack_army_actions", :force => true do |t|
     t.integer  "attacker_id"
