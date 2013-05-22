6acb5f1c2a49f583904dd5d9823c559a3eeb3fd7
salange <sascha@5dlab.com>
2013-05-22 20:05:42 +0200

    no tracking

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.6.0
+# Version: 0.6.1
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -115,9 +115,9 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 6,
-                    :build => 0,
+                    :build => 1,
       },
-      :app_control => { :debug_tracking => 1,
+      :app_control => { :debug_tracking => 0,
       },
       :battle => {
         :calculation => {
