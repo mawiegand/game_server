e530f00878b9268a58252d38e4c2d05f71efaa15
Christian Wansart <christian@5dlab.com>
2013-06-03 11:16:57 +0200

    added avatar_string migration

---
+++ db/migrate/20130602100329_add_avatar_string_to_fundamental_character.rb
@@ -0,0 +1,12 @@
+require 'game_state/avatars'
+
+class AddAvatarStringToFundamentalCharacter < ActiveRecord::Migration
+  def change
+    add_column :fundamental_characters, :avatar_string, :string
+
+    avatar = GameState::Avatars.new
+    Fundamental::Character.all.each do |char|
+      char.avatar_string = avatar.create_random_avatar_string(char.male?)
+    end
+  end
+end
