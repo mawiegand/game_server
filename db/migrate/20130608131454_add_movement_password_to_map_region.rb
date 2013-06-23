d11c1a6eefe05b1af24666f966b286fde6f2346b
Marcel Wiegand <marcel@bluepanel.org>
2013-06-23 21:07:23 +0200

    Fix migrations for moving settlements

--- db/migrate/20130608131454_add_movement_password_to_map_region.rb
+++ db/migrate/20130608131454_add_movement_password_to_map_region.rb
@@ -1,10 +1,6 @@
 class AddMovementPasswordToMapRegion < ActiveRecord::Migration
   def up
     add_column :map_regions, :movement_password, :string
-
-    Map::Region.all.each do |region|
-      region.save
-    end
   end
 
   def down
