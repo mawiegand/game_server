18078cca0769d7b5510e831110be8aa88849f110
Patrick <p@trick-fox.de>
2013-06-25 02:34:37 +0200

    corrected access rights of special offers controller

--- app/controllers/shop/special_offers_controller.rb
+++ app/controllers/shop/special_offers_controller.rb
@@ -2,7 +2,8 @@ class Shop::SpecialOffersController < ApplicationController
   layout 'shop'
 
   before_filter :authenticate
-  before_filter :deny_api
+  before_filter :deny_api,        :except => [:show, :index]
+  before_filter :authorize_staff, :except => [:show, :index]
 
   # GET /shop/special_offers
   # GET /shop/special_offers.json
