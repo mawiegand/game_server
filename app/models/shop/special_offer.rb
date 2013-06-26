e1e9d3961374ec4bf74e2959892969bb31813247
Patrick <p@trick-fox.de>
2013-06-26 02:19:27 +0200

    get special offer of purchase

--- app/models/shop/special_offer.rb
+++ app/models/shop/special_offer.rb
@@ -1,7 +1,5 @@
 class Shop::SpecialOffer < ActiveRecord::Base
 
-  has_many :purchases, :class_name => "Shop::Purchase", :foreign_key => "offer_id", :inverse_of => :special_offer
-
   def credit_to(character)
     logger.debug "--->credited special_offer to character " + character.inspect
   end
