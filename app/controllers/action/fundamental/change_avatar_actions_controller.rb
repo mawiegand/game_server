5cde4acd36b3a5860f8a3f912763ca73eb987ca5
Christian Wansart <christian@5dlab.com>
2013-06-27 22:40:19 +0200

    added create method

--- app/controllers/action/fundamental/change_avatar_actions_controller.rb
+++ app/controllers/action/fundamental/change_avatar_actions_controller.rb
@@ -11,11 +11,22 @@ class Action::Fundamental::ChangeAvatarActionsController < ApplicationController
     new_avatar_string = avatar.create_random_avatar_string(current_character.gender == 'male')
 
     respond_to do |format|
-      format.json { render json: {'avatar_string' => new_avatar_string, status: :ok }
+      format.json { render json: {'avatar_string' => new_avatar_string}, status: :ok }
+      format.text { render text: new_avatar_string, status: :ok }
     end
   end
 
   # save avatar
   def create
+    raise BadRequestError.new('no current character') if current_character.nil?
+    raise BadRequestError.new('missing parameter(s)') if params[:avatar_string].nil?
+
+    #check if avatar string is conform
+    current_character.avatar_string = params[:avatar_string]
+    current_character.save
+
+    respond_to do |format|
+      format.json { render json: {}, status: :ok }
+    end 
   end
 end
