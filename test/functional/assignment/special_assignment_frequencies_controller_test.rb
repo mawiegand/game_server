cd17fec49a1bfc843e191c01944dd27113798123
Patrick <p@trick-fox.de>
2013-07-02 14:48:49 +0200

    bugfix: added user in tests

--- test/functional/assignment/special_assignment_frequencies_controller_test.rb
+++ test/functional/assignment/special_assignment_frequencies_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Assignment::SpecialAssignmentFrequenciesControllerTest < ActionController::TestCase
   setup do
     @assignment_special_assignment_frequency = assignment_special_assignment_frequencies(:one)
+    @controller.current_backend_user = backend_users(:staff)  # this is a quick hack to make the scaffolded tests pass. Must be moved to individual tests later.
   end
 
   test "should get index" do
--- test/functional/assignment/special_assignments_controller_test.rb
+++ test/functional/assignment/special_assignments_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Assignment::SpecialAssignmentsControllerTest < ActionController::TestCase
   setup do
     @assignment_special_assignment = assignment_special_assignments(:one)
+    @controller.current_backend_user = backend_users(:staff)  # this is a quick hack to make the scaffolded tests pass. Must be moved to individual tests later.
   end
 
   test "should get index" do
