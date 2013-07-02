b20234b949e81b444c5dee2dd61ddc0b952ebc38
Patrick <p@trick-fox.de>
2013-07-02 15:44:30 +0200

    bugfix

--- app/controllers/shop/special_offers_controller.rb
+++ app/controllers/shop/special_offers_controller.rb
@@ -16,7 +16,7 @@ class Shop::SpecialOffersController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: include_root(@shop_special_offers, :shop_special_offer)  }
+      format.json { render json: include_root(@shop_special_offers, :special_offer)  }
     end
   end
 
