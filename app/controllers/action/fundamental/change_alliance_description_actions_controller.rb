f2d782ef2b7b7fba657f9d36aee89d7e8dba7329
salange <sascha@5dlab.com>
2013-07-14 22:41:52 +0200

    integer alliance id

--- app/controllers/action/fundamental/change_alliance_description_actions_controller.rb
+++ app/controllers/action/fundamental/change_alliance_description_actions_controller.rb
@@ -11,7 +11,7 @@ class Action::Fundamental::ChangeAllianceDescriptionActionsController < Applicat
     raise BadRequestError.new('missing parameter(s)') if params[:change_alliance_description_action].nil? || params[:change_alliance_description_action][:alliance_id].blank? || params[:change_alliance_description_action][:description].blank?
 
     raise ForbiddenError.new('tried to do a leader action although not even in an alliance') if current_character.alliance_id.blank?
-    raise ForbiddenError.new('tried to do change wrong alliance') unless current_character.alliance_id == params[:change_alliance_description_action][:alliance_id]
+    raise ForbiddenError.new('tried to do change wrong alliance') unless current_character.alliance_id == params[:change_alliance_description_action][:alliance_id].to_id
     raise ForbiddenError.new('only leader can kick member') unless current_character.alliance_leader?
     
     alliance = currenct_character.alliance
