efbb57ff791842060468b10bbbed603d7762ac4b
salange <sascha@5dlab.com>
2013-07-14 22:00:52 +0200

    different root

--- app/controllers/action/fundamental/change_alliance_description_actions_controller.rb
+++ app/controllers/action/fundamental/change_alliance_description_actions_controller.rb
@@ -8,13 +8,14 @@ class Action::Fundamental::ChangeAllianceDescriptionActionsController < Applicat
   def create
 
     raise BadRequestError.new('no current character') if current_character.nil?
-    raise BadRequestError.new('missing parameter(s)') if params[:alliance].nil? || params[:alliance][:description].blank?
+    raise BadRequestError.new('missing parameter(s)') if params[:change_alliance_description_action].nil? || params[:change_alliance_description_action][:id].blank? || params[:change_alliance_description_action][:description].blank?
 
     raise ForbiddenError.new('tried to do a leader action although not even in an alliance') if current_character.alliance_id.blank?
+    raise ForbiddenError.new('tried to do change wrong alliance') unless current_character.alliance_id == params[:change_allience_description_action][:id]
     raise ForbiddenError.new('only leader can kick member') unless current_character.alliance_leader?
     
     alliance = currenct_character.alliance
-    alliance.description = params[:alliance][:description].strip
+    alliance.description = params[:change_allience_description_action][:description].strip
 
     raise BadRequestError.new('saving the alliance description did fails.')  unless alliance.save
 
