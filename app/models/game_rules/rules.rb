e735aea6237b728204ea33c9b07f8d599786d044
Patrick <p@trick-fox.de>
2013-07-16 22:34:21 +0200

    raise version number and make

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.9.14
+# Version: 0.9.15
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -119,7 +119,7 @@ class GameRules::Rules
   
       :version => { :major => 0,
                     :minor => 9,
-                    :build => 14,
+                    :build => 15,
       },
       :app_control => {
         :debug_tracking       => 1,
@@ -7505,7 +7505,7 @@ Hinter der Häuptlingshütte ist ein kleiner Lagerplatz, auf dem Rohstoffe zwisc
             :minimum_sacred_rank => 0,
             :name        => {
               
-            :de_DE => "Nachwuchs-Höhlenlöwenschwichtiger",
+            :de_DE => "Nachwuchs-Höhlenlöwenbeschwichtiger",
   
             :en_US => "Junior-Cavelionappeaser",
   
