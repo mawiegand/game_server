816a93f382d6287300f6a7794fba43f289b47760
Patrick <p@trick-fox.de>
2013-07-16 16:54:31 +0200

    bugfix: removed old code

--- app/controllers/action/assignment/start_special_assignment_actions_controller.rb
+++ app/controllers/action/assignment/start_special_assignment_actions_controller.rb
@@ -13,12 +13,6 @@ class Action::Assignment::StartSpecialAssignmentActionsController < ApplicationC
     raise BadRequestError.new('assignment must not be started')  unless @assignment.started_at.nil?
     raise BadRequestError.new('assignment must not be ended')    unless @assignment.ended_at.nil?
 
-    # assume the tavern is at home base (otherwise the settlement id must be send to controller to identifiy the settlement)
-    requirement_groups = @assignment.assignment_type[:requirementGroups]
-    requirements_met = requirement_groups.nil? || requirement_groups.empty? || GameState::Requirements.meet_one_requirement_group?(requirement_groups, current_character, current_character.home_location.settlement)
-    raise BadRequestError.new('requirements not met')            unless requirements_met
-
-
     level = current_character.assignment_level || 0
     raise ForbiddenError.new('Character cannot solve assignments of this level')  if level < @assignment.assignment_type[:level]
 
