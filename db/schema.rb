e633df25f7babc2ae4e59191c710ca852d9d3a4d
Patrick <p@trick-fox.de>
2013-08-06 17:10:59 +0200

    local db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1903,15 +1903,15 @@ ActiveRecord::Schema.define(:version => 20130805121659) do
     t.datetime "insider_since"
     t.boolean  "first_round"
     t.datetime "tutorial_finished_at"
-    t.decimal  "construction_bonus_effect",                :default => 0.0,   :null => false
-    t.decimal  "construction_bonus_total",                 :default => 0.0,   :null => false
     t.integer  "assignment_level",                         :default => 0,     :null => false
-    t.datetime "premium_expiration_displayed_at"
-    t.integer  "special_offer_dialog_count",               :default => 0
-    t.datetime "special_offer_displayed_at"
-    t.boolean  "logged_in_on_second_day",                  :default => false, :null => false
     t.datetime "moved_at"
     t.integer  "old_base_location_id"
+    t.integer  "special_offer_dialog_count",               :default => 0
+    t.datetime "premium_expiration_displayed_at"
+    t.datetime "special_offer_displayed_at"
+    t.boolean  "logged_in_on_second_day",                  :default => false, :null => false
+    t.decimal  "construction_bonus_effect",                :default => 0.0,   :null => false
+    t.decimal  "construction_bonus_total",                 :default => 0.0,   :null => false
     t.text     "description"
   end
 
