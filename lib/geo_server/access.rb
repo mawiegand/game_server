f24f41dd709d4c3a7a59cbecd5611164acbe04c1
Patrick <p@trick-fox.de>
2013-08-16 12:39:59 +0200

    object for accessing geo server

---
+++ lib/geo_server/access.rb
@@ -0,0 +1,37 @@
+require 'five_d/access_token'
+require 'httparty'
+
+module GeoServer
+
+  class Access
+    
+    def initialize(attributes = {})
+      @attributes = attributes
+    end
+
+    def fetch_fundamental_character_self
+      get('/fundamental/characters/self')
+    end
+
+    def open_treasure(treasure_id, current_identifier)
+      post('/action/treasure/open_treasure_actions', {:id => treasure_id, :identifier => current_identifier})
+    end
+
+    protected
+      
+      def post(path, body = {})
+        HTTParty.post(@attributes[:geo_server_provider_base_url] + path,
+                      :body => body, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
+      end
+  
+      def put(path, body = {})
+        HTTParty.put(@attributes[:geo_server_provider_base_url] + path,
+                     :body => body, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
+      end
+  
+      def get(path, query = {})
+        HTTParty.get(@attributes[:geo_server_provider_base_url] + path,
+                     :query => query, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
+      end
+  end
+end
