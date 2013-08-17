9454753722fb34b0efb720600a5431d3ecd2efc7
Patrick <p@trick-fox.de>
2013-08-17 20:08:21 +0200

    bugfix

--- lib/geo_server/access.rb
+++ lib/geo_server/access.rb
@@ -14,24 +14,24 @@ module GeoServer
     end
 
     def open_treasure(treasure_id, current_identifier)
-      response = post('/action/treasure/open_treasure_actions', {:id => treasure_id, :identifier => current_identifier})
+      response = post('https://test1.wack-a-doo.de/geo_server/action/treasure/open_treasure_actions', {:open_treasure_action => {:id => treasure_id, :identifier => current_identifier}})
       response.code == 200
     end
 
     protected
       
       def post(path, body = {})
-        HTTParty.post(GAME_SERVER_CONFIG[:geo_server_base_url] + path,
+        HTTParty.post(GAME_SERVER_CONFIG['geo_server_base_url'] + path,
                       :body => body, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
       end
   
       def put(path, body = {})
-        HTTParty.put(GAME_SERVER_CONFIG[:geo_server_base_url] + path,
+        HTTParty.put(GAME_SERVER_CONFIG['geo_server_base_url'] + path,
                      :body => body, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
       end
   
       def get(path, query = {})
-        HTTParty.get(GAME_SERVER_CONFIG[:geo_server_base_url] + path,
+        HTTParty.get(GAME_SERVER_CONFIG['geo_server_base_url'] + path,
                      :query => query, :headers => { 'Accept' => 'application/json', 'Authorization' => "Bearer #{ @attributes[:auth_token] }"})
       end
   end
