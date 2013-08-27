6ac3322ce5c1bf8aba78d359cbe27107989bff0a
Patrick <p@trick-fox.de>
2013-08-27 19:04:50 +0200

    current insider ingame message

--- script/send_insider_message.rb
+++ script/send_insider_message.rb
@@ -11,54 +11,52 @@ STDOUT.sync = true
 puts
 puts "This script sends all players a notification about their new insider status."
 
- sender = Fundamental::Character.find_by_name('Sascha')
+ sender = Fundamental::Character.find_by_name('WackyUser143')
  
 if !sender.nil? 
 
  Fundamental::Character.non_npc.not_deleted.each do |character|
    if character.insider
      
-     male = character.male? 
-     
+     male = character.male?
+
      Messaging::Message.create({
        type_id:    Messaging::Message::INDIVIDUAL_ANNOUNCEMENT_TYPE_ID,
        sender_id:  sender.id,
        recipient_id: character.id,
-       subject:   "Wack-A-Doo Insider!",
-       body:      "<p><small>Please find an English summary at the end of this message.</small></p>\
-<h1>Hallo #{ character.name },</h1>\
-<p>als #{ male ? "einem Spieler" : "einer Spielerin" } der ersten Stunde haben wir Dir und allen anderen aktiven #{ male ? "Spielern" : "Spielerinnen" } den Status\
-   <b>Insider</b> verliehen. Dies ist als Dankeschön für die frühe Teilnahme gedacht und bleibt\
-   Dir dauerhaft erhalten. Nachfolgende #{ male ? "Spieler" : "Spielerinnen" } können ebenfalls in diesen erlauchten Kreis\
-   aufrücken, indem sie eine Runde von Wack-A-Doo gewinnen oder mindestens drei Runden\
-   überleben.</p>\
-<p>Als Insider kannst Du ab sofort einen gesonderten Chatraum betreten und wirst
-   in Zukunft regelmäßig News über die Spielentwicklung in einem (optionalen) Newsletter 
-   beziehen können. Der Chatraum ist als ruhiges Refugium für erfahrene Spieler gedacht,
-   ab sofort im Browser (https://wack-a-doo.de) verfügbar und wird auf iOS mit
-   dem nächsten Update der App freigeschaltet. Über den Newsletter werden wir in den 
-   nächsten Wochen gesondert informieren.</p> 
-<p>Gerne lass uns Dein Feedback zukommen. Insbesondere die ersten 
-   Erfahrungen unserer neuen App User interessieren uns brennend. Die App ist noch in einem
-   recht frühen Stadium, aber wir arbeiten ununterbrochen an Verbesserungen und Erweiterungen
-   - natürlich auch für den Browser.</p>
-<p>Bitte wende Dich mit 
-   Feedback oder auch im Falle von Fragen und Problemen einfach
-   direkt an mich oder an meine Kollegen bei 5d lab (Allianz: 5D) oder per Email an 
-   <a href=\"mailto:support@5dlab.com\">support@5dlab.com</a>.</p>
-<p>Abschließend möchte ich Dich noch einmal herzlich in Wack-A-Doo willkommen heißen: 
-   Wir freuen uns, dass Du da bist!</p>
-<p>Viel Spaß<br/>und<br/>Whack On<br/><br/>Sascha.</p>
-<br/><br/><br/><hr/>
-<h1>Hi #{ character.name },</h1>\
-<p>this message was sent to let you know about your new status \"Wack-A-Doo Insider\".
-   We granted this status to all active players as a small thank you for joining and playing
-   Wack-A-Doo that early. Your status allows you to access an exclusive chat room in the
-   browser client and, in about a week, in the iOS client.</p>
-<p>Furthermore, we'd like to welcome you again to the game and to encourage you to contact
-   us in case of questions or problems - or just to give us some valuable feedback.</p>
-<p>Have fun and Whack On<br>Sascha.</p> 
-",
+       subject:   "Start Next, Rundenende & Entwicklerchat",
+       body:      "<h2>Hallo #{ character.name },</h2>
+
+<p>wir möchten Euch heute auf unsere still und leise gestartete Crowdfunding-Kampagne auf <a href='http://www.startnext.de/wack-a-doo' target='_blank'>Startnext</a> aufmerksam machen. Bis zum 9.9. könnt Ihr mit Euer Teilnahme helfen, die kommende Runde zu starten und Wack-A-Doo weiter zu entwickeln.</p>
+
+<p>Besonders hinweisen möchten wir auf das <b>Startpaket für Runde 4</b>, quasi das <b>Rundenticket</b> für die kommende Runde. Ihr erhaltet für eine Unterstützung in Höhe von 15 EUR einen Bonus von 6000 Startressourcen sowie 100 Credits. Dies ist ein kleiner Bonus in Runde 4 gegenüber den nicht unterstützende Spielern, der nur auf diese Weise bezogen werden kann. Namentlich verewigt (Nickname oder Realname) werdet Ihr natürlich auch!</p>
+
+<p>Tassen und T-Shirts gibt‘s natürlich auch; in und ohne Verbindung mit einem lebenslangem Platinum-Account.</p>
+
+<p>Die Teilnahme ist dabei absolut risikolos, wird das Finanzierungsziel nicht erreicht, bekommt Ihr Euer Geld automatisch zurückerstattet.</p>
+
+<p>Wir sind sehr zuversichtlich das Ziel von 3.000 Euro zu erreichen. 100 Leute, die das Starterpaket erwerben, führen z.B. schon zum Erreichen der Finanzierungshürde. Mit Euer zahlreichen Unterstützung schaffen wir vielleicht sogar noch die nächste Stufe und können die Neugestaltung des Neandertalers bei <a href='http://www.alexanderpierschel.de' target='_blank'> Pikomi </a> in Auftrag geben.</p>
+
+
+<h2>Rundenende aktuelle Runde</h2>
+
+<p>Wir läuten in dieser Woche das Rundenende ein. Ab sofort wird der Prozentsatz für den <b>Herrschaftssieg</b> schneller sinken. Weitere Artefakte werden im Laufe der kommenden Tage im Spiel auftauchen. Genauere Infos gibt es beim freundlichen Neandertaler.</p>
+
+<p>Wir vergeben zum Rundenende außerdem wieder verschiedene Belohnungen, die am Spielercharakter dauerhaft für <b>alle weiteren Runden</b> vermerkt bleiben:
+</p>
+
+<ul><li>Jeder Spieler der <b>Allianz</b>, die zuerst eine der beiden Siegbedingungen erfüllt, erhält neben dem Eintrag als Rundensieger einen dauerhaften <b>10% Bonus auf alle erhaltene Erfahrung</b>.</li>
+<li>Alle Spieler der Top 100 erhalten einen <b>dauerhaften 5% Bonus auf die Erfahrung</b>. (Spieler der Siegerallianz in Summe 15% Bonus!)</li>
+<li>Alle Spieler der Top 500 erhalten den Eintrag „Runde Erde überlebt“ in ihr Profil.</li>
+<li>Wir speichern außerdem bereits seit Runde 0 alle Rankingpositionen und werden sie innerhalb der nächsten Runde am Profil einsehbar machen. D.h. jede Verbesserung im Ranking wird notiert und zählt ;-)</li>
+</ul>
+<h2>Chat</h2>
+<p>Heute, am Dienstag um 20.00 Uhr findet ein allgemeiner, offener Entwicklerchat statt. Wenn Ihr Zeit habt, schaut in global vorbei; vielleicht bekommt Ihr Eure Frage unter.</p>
+
+<p>Weiterhin viel Spaß<br/>
+Wack On<br/>
+<br/>
+Euer Wack-A-Doo Team</p>",
        send_at:   DateTime.now,
        reported:  false,
        flag:      0,
