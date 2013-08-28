0bcc3206551c9bac43af47f21e386fdae2a0031e
Patrick <p@trick-fox.de>
2013-08-28 12:12:37 +0200

    removed debug output

--- app/controllers/settlement/slots_controller.rb
+++ app/controllers/settlement/slots_controller.rb
@@ -59,8 +59,6 @@ class Settlement::SlotsController < ApplicationController
             raise ForbiddenError.new('Access Forbidden')        
           end  
           
-          logger.debug "SLOTS: #{@settlement_slots.inspect}"
-          
           if params.has_key?(:short)
             render json: @settlement_slots, :only => @@short_fields
           elsif params.has_key?(:aggregate)
