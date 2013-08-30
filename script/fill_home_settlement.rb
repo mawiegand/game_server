85b0494f8d733687bb89b6c1f7a57bbaf2c41c79
Jan Kegel <jkegel@ml31.informatik.uni-freiburg.de>
2013-08-30 17:14:03 +0200

    added correct id's and amounts to dill_home_settlement

--- script/fill_home_settlement.rb
+++ script/fill_home_settlement.rb
@@ -34,26 +34,187 @@ def credit_to(character)
   fur_id          =    2
   cash_id         =    3
   cash_amount     =  300
-  resource_amount = 5000
+  resource_amount = 75000
     
   slots = {
     1 => {
-      id: 21,
-      level: 10,
+      id: 2,
+      level: 20,
     },
     2 => {
-      id: 23,
-      level: 1,
+      id: 0,
+      level: 20,
     },
+    3 => {
+      id: 19,
+      level: 18,
+    },
+    4 => {
+      id: 20,
+      level: 18,
+    },
+    5 => {
+      id: 16,
+      level: 10,
+    },
+    6 => {
+      id: 15,
+      level: 10,
+    },
+    7 => {
+      id: 18,
+      level: 10,
+    },
+    8 => {
+      id: 16,
+      level: 10,
+    },
+    9 => {
+      id: 15,
+      level: 10,
+    },
+    10 => {
+      id: 18,
+      level: 10,
+    },
+    11 => {
+      id: 2,
+      level: 10,
+    },
+    12 => {
+      id: 9,
+      level: 10,
+    },
+    13 => {
+      id: 17,
+      level: 10,
+    },
+    14 => {
+      id: 17,
+      level: 10,
+    },
+    15 => {
+      id: 2,
+      level: 10,
+    },
+    16 => {
+      id: 12,
+      level: 10,
+    },
+    17 => {
+      id: 12,
+      level: 10,
+    },
+    18 => {
+      id: 30,
+      level: 10,
+    },
+    19 => {
+      id: 8,
+      level: 10,
+    },
+    20 => {
+      id: 2,
+      level: 10,
+    },
+    21 => {
+      id: 12,
+      level: 10,
+    },
+    22 => {
+      id: 17,
+      level: 10,
+    },
+    23 => {
+      id: 14,
+      level: 10,
+    },
+    24 => {
+      id: 5,
+      level: 10,
+    },
+    25 => {
+      id: 11,
+      level: 10,
+    },
+    26 => {
+      id: 14,
+      level: 10,
+    },
+    27 => {
+      id: 13,
+      level: 10,
+    },
+    28 => {
+      id: 14,
+      level: 10,
+    },
+    29 => {
+      id: 16,
+      level: 10,
+    },
+    30 => {
+      id: 15,
+      level: 10,
+    },
+    31 => {
+      id: 18,
+      level: 10,
+    },
+    32 => {
+      id: 14,
+      level: 10,
+    },
+    33 => {
+      id: 29,
+      level: 4,
+    },
+    34 => {
+      id: 14,
+      level: 10,
+    },
+    35 => {
+      id: 16,
+      level: 10,
+    },
+    36 => {
+      id: 15,
+      level: 10,
+    },
+    37 => {
+      id: 18,
+      level: 10,
+    },
+    38 => {
+      id: 16,
+      level: 10,
+    },
+    39 => {
+      id: 15,
+      level: 10,
+    },
+    40 => {
+      id: 18,
+      level: 10,
+    },
+    
   }
   
   units = [
     {
-      id: 0,
-      number: 20,
+      id: 3,
+      number: 250,
+    },
+    {
+      id: 7,
+      number: 250,
+    },
+    {
+      id: 11,
+      number: 250,
     },
   ]
-      
+  
   settlement = character.bases[0]
   
   slots.each do |slot_num, specs|
