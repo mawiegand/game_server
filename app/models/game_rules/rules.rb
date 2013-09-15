05a62aa9575ab75771eaca135ee3fb4e8affb890
Patrick <p@trick-fox.de>
2013-09-15 23:00:37 +0200

    two new artifacts

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 1.1.8
+# Version: 1.1.9
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -121,7 +121,7 @@ class GameRules::Rules
   
       :version => { :major => 1,
                     :minor => 1,
-                    :build => 8,
+                    :build => 9,
       },
       :app_control => {
         :debug_tracking                         => 1,
@@ -8110,7 +8110,7 @@ Hinter der Häuptlingshütte ist ein kleiner Lagerplatz, auf dem Rohstoffe zwisc
   
           },
 
-          :amount      => '0',
+          :amount      => '2',
 
           :construction_bonus  => [
 
