d8bdc59cb94af7c2556ea87fc3480fc002175a90
Patrick <p@trick-fox.de>
2013-09-24 11:29:08 +0200

    connect bonus offer and resource effect via origin id

--- app/models/effect/resource_effect.rb
+++ app/models/effect/resource_effect.rb
@@ -2,6 +2,7 @@ class Effect::ResourceEffect < ActiveRecord::Base
   
   has_one    :event,     :class_name => "Event::Event", :foreign_key => "local_event_id",  :dependent => :destroy, :conditions => "event_type = 'resource_effect'"
   belongs_to :resource_pool, :class_name => "Fundamental::ResourcePool", :foreign_key => "resource_pool_id", :inverse_of => :resource_effects
+  belongs_to :bonus_offer,   :class_name => "Shop::BonusOffer",          :foreign_key => "origin_id"
 
   RESOURCE_EFFECT_TYPE_SHOP = 0
   RESOURCE_EFFECT_TYPE_ARTIFACT = 1
@@ -10,15 +11,16 @@ class Effect::ResourceEffect < ActiveRecord::Base
   before_destroy :propagate_effect_removal
   
   
-  def self.create_or_extend_shop_effect(character, resource_id, bonus, duration)
+  def self.create_or_extend_shop_effect(character, resource_id, bonus, duration, origin_id)
     # check if same effects is already active
-    effects = Effect::ResourceEffect.where(:resource_pool_id => character.resource_pool.id, 
-                                           :type_id => Effect::ResourceEffect::RESOURCE_EFFECT_TYPE_SHOP, 
-                                           :resource_id => resource_id)
+    effects = Effect::ResourceEffect.where(
+      :resource_pool_id => character.resource_pool.id,
+      :type_id => Effect::ResourceEffect::RESOURCE_EFFECT_TYPE_SHOP,
+      :resource_id => resource_id,
+      :origin_id => origin_id
+    )
     
     raise BadRequestError.new('more than one bonus active for same resource') if effects.count > 1
-    
-    effect = nil
     
     if effects.count == 1
       effect = effects.first
@@ -27,13 +29,14 @@ class Effect::ResourceEffect < ActiveRecord::Base
       
       event = effect.event
       raise BadRequestError.new('no event for existing effect') if event.nil?
-      event.execute_at   += duration * 3600   # TODO: why not event.execute_at = effect.finished_at ???
+      event.execute_at   += duration * 3600
       event.save
     else
       effect = Effect::ResourceEffect.create({
         bonus: bonus,
         resource_pool_id: character.resource_pool.id,
         resource_id: resource_id,
+        origin_id: origin_id,
         type_id: Effect::ResourceEffect::RESOURCE_EFFECT_TYPE_SHOP,
         finished_at: Time.now + (duration * 3600),
       })
@@ -41,7 +44,7 @@ class Effect::ResourceEffect < ActiveRecord::Base
       # event for effect
       effect.create_event(
         character: character,
-        execute_at: effect.finished_at,   # Ah, here it's used!
+        execute_at: effect.finished_at,
         event_type: "resource_effect",
         local_event_id: effect.id,
       )
--- app/models/shop/bonus_offer.rb
+++ app/models/shop/bonus_offer.rb
@@ -1,18 +1,19 @@
 class Shop::BonusOffer < ActiveRecord::Base
-  
+
   def effect_for_character(character)
-    effects = Effect::ResourceEffect.where(:resource_pool_id => character.resource_pool.id, :type_id => Effect::ResourceEffect::RESOURCE_EFFECT_TYPE_SHOP, :resource_id => self.resource_id)
+    effects = Effect::ResourceEffect.where(
+      :resource_pool_id => character.resource_pool.id,
+      :type_id => Effect::ResourceEffect::RESOURCE_EFFECT_TYPE_SHOP,
+      :resource_id => self.resource_id,
+      :origin_id => self.id
+    )
     raise BadRequestError.new('more than one bonus active for same resource') if effects.count > 1
     
-    if effects.count == 1
-      return effects.first
-    else
-      return nil
-    end
+    effects.first
   end
   
   def credit_to(character)
-    Effect::ResourceEffect.create_or_extend_shop_effect(character, self.resource_id, self.bonus, self.duration)
+    Effect::ResourceEffect.create_or_extend_shop_effect(character, self.resource_id, self.bonus, self.duration, self.id)
   end
   
 end
