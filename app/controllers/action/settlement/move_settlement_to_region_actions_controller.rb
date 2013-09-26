303bef44f7458527da8bc9d575d4ee1e01940538
Patrick <p@trick-fox.de>
2013-09-26 17:59:34 +0200

    cleaned up error status codes

--- app/controllers/action/settlement/move_settlement_to_region_actions_controller.rb
+++ app/controllers/action/settlement/move_settlement_to_region_actions_controller.rb
@@ -8,16 +8,17 @@ class Action::Settlement::MoveSettlementToRegionActionsController < ApplicationC
   	raise BadRequestError.new('already moved') if current_character.moved_home_settlement?
     
     region = Map::Region.find_by_name(params[:move_settlement_action][:region_name])
-    raise NotFoundError.new('region not existing') if region.nil?
+    raise BadRequestError.new('region not existing') if region.nil?
+    raise ConflictError.new("fortress in target region is fighting") if region.fortress.fighting?
     raise ForbiddenError.new("already exists one settlement in region") unless region.settleable_by?(current_character)
-    raise BadRequestError.new("moving is not allowed") unless region.is_moving_allowed?(current_character.alliance, params[:move_settlement_action][:region_password])
+    raise UnprocessableEntityError.new("moving is not allowed") unless region.is_moving_allowed?(current_character.alliance, params[:move_settlement_action][:region_password])
     raise ConflictError.new("region was captured less than 12 hours ago") if !region.last_takeover_at.nil? and region.last_takeover_at >= GAME_SERVER_CONFIG['moving_disable_time_after_region_takeover'].hours.ago
 
     old_base_location = Map::Location.find(current_character.base_location_id)
 		current_character.old_base_location_id = current_character.base_location_id
 
     home_base = Settlement::Settlement.find_by_location_id(current_character.base_location_id)
-    raise ConflictError.new("home_base is fighting") if home_base.fighting?
+    raise BadRequestError.new("home_base is fighting") if home_base.fighting?
     home_base.move_settlement_to_region(region)
     current_character.base_location_id = home_base.location_id
     current_character.base_region_id = home_base.location.region_id
