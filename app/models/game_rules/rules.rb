8d9b0a2a6080780c0b51624e166a03e520f1d070
Jan Kegel <jkegel@ml31.informatik.uni-freiburg.de>
2013-10-01 14:30:00 +0200

    Changed Neanderthal XP factor back to 1

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 1.3.1
+# Version: 1.3.2
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -122,7 +122,7 @@ class GameRules::Rules
   
       :version => { :major => 1,
                     :minor => 3,
-                    :build => 1,
+                    :build => 2,
       },
       :app_control => {
         :debug_tracking                         => 1,
@@ -2398,7 +2398,7 @@ class GameRules::Rules
                 
           },
 
-          :experience_factor => 0.0,
+          :experience_factor => 1.0,
           :trainable   => false,
 
           :velocity    => 1,
