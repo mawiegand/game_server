bfbed4024e195b74dde1bee27b08be3854a76c7c
Patrick <p@trick-fox.de>
2013-10-03 23:50:40 +0200

    return challenge

--- app/controllers/shop/fb_payments_controller.rb
+++ app/controllers/shop/fb_payments_controller.rb
@@ -2,8 +2,10 @@ require 'credit_shop'
 
 class Shop::FbPaymentsController < ApplicationController
 
-  def callback
+  VERIFY_TOKEN = 'UKUKvzHHAg8gjXynx3hioFX7nC8KLa'
 
+  def callback
+    render text: params['hub.challenge']
   end
 
 end
