82d465a7f571623dea729053b5fa94220f6fc5ce
Patrick <p@trick-fox.de>
2013-10-04 14:48:59 +0200

    implement verify fb order action to book credits

--- app/controllers/action/shop/fb_verify_order_actions_controller.rb
+++ app/controllers/action/shop/fb_verify_order_actions_controller.rb
@@ -1,39 +1,70 @@
+require 'credit_shop'
+
 class Action::Shop::FbVerifyOrderActionsController < ApplicationController
   layout 'action'
 
   before_filter :authenticate
 
+  FB_APP_ID        = '127037377498922'
+  FB_APP_SECRET    = 'f88034e6df205b5aa3854e0b92638754'
+  FB_CREDIT_AMOUNT = 30
+
+  BYTRO_URL_BASE       = 'https://secure.bytro.com/index.php'
+  BYTRO_SHARED_SECRET  = 'jfwjhgflhg254tp9824ghqlkgjh25pg8hgljkgh5896ogihdgjh24uihg9p8zgagjh2p895ghfsjgh312g09hjdfghj'
+  BYTRO_KEY            = 'wackadoo'
+
   def create
-    #raise BadRequestError.new('no current character') if current_character.nil?
-    #raise BadRequestError.new('missing parameter(s)') if params[:action_redeem_purchase_action].nil? || params[:action_redeem_purchase_action][:external_offer_id].blank?
-    #
-    #special_offer = Shop::SpecialOffer.find_by_external_offer_id(params[:action_redeem_purchase_action][:external_offer_id])
-    #raise NotFoundError.new ("special offer with offer id #{params[:action_redeem_purchase_action][:external_offer_id]} not Found.") if special_offer.nil?
-    #
-    #ActiveRecord::Base.transaction(:requires_new => true) do
-    #
-    #  shop_special_offers_transaction = Shop::SpecialOffersTransaction.find_or_create_by_character_id_and_external_offer_id(current_character.id, special_offer.external_offer_id)
-    #  shop_special_offers_transaction.lock!
-    #
-    #  if shop_special_offers_transaction.purchase.nil?
-    #    purchase = shop_special_offers_transaction.create_purchase({
-    #                                                                   character_id: current_character.id,
-    #                                                                   external_offer_id: special_offer.external_offer_id,
-    #                                                               })
-    #  else
-    #    purchase = shop_special_offers_transaction.purchase
-    #  end
-    #
-    #  if !purchase.redeemed? && !shop_special_offers_transaction.redeemed?
-    #    purchase.special_offer.credit_to(current_character)
-    #    purchase.redeemed_at = Time.now
-    #    purchase.save!
-    #
-    #    shop_special_offers_transaction.redeemed_at = Time.now
-    #    shop_special_offers_transaction.state = Shop::Transaction::STATE_REDEEMED
-    #    shop_special_offers_transaction.save!
-    #  end
-    #end
+
+    payment_id = params['fb_verify_order_actions'] && params['fb_verify_order_actions']['payment_id']
+    signed_request = params['fb_verify_order_actions'] && params['fb_verify_order_actions']['signed_request']
+
+    if !payment_id.blank? && !signed_request.blank?
+
+      response = HTTParty.get("https://graph.facebook.com/#{payment_id}", :query => {access_token: "#{FB_APP_ID}|#{FB_APP_SECRET}"})
+
+      if response.code == 200
+
+        parsed_response = response.parsed_response
+        action = parsed_response['actions'][0]
+
+        if action['status'] == 'completed'
+
+          fb_user_id = parsed_response['user'] && parsed_response['user']['id']
+          identity = Identity.find_by_fb_player_id(fb_user_id)
+
+          unless identity.nil?
+            data = {
+                userID:      identity.identifier,
+                method:      'bytro',
+                offerID:     '249',
+                scaleFactor: FB_CREDIT_AMOUNT.to_s,
+                tstamp:      Time.now.to_i.to_s,
+                comment:     '1',
+                # comment: Base64.encode64(virtual_bank_transaction[:transaction_id].to_s).gsub(/[\n\r ]/,'')  # Hack!
+            }
+
+            query = {
+                eID:    'api',
+                key:    CreditShop::BytroShop::KEY,
+                action: 'processPayment',
+                data:   CreditShop::BytroShop.encoded_data(data),
+            }
+
+            query = CreditShop::BytroShop.add_hash(query)
+            http_response = HTTParty.post(CreditShop::BytroShop::URL_BASE, :query => query)
+
+            if http_response.code === 200
+              api_response = http_response.parsed_response
+              api_response = JSON.parse(api_response) if api_response.is_a?(String)
+              if api_response['resultCode'] === 0
+                render json: :ok
+                return
+              end
+            end
+          end
+        end
+      end
+    end
 
     respond_to do |format|
       format.json { render json: {}, status: :ok }
