fb56eac802a7ae44ac26712eeb525571bfa0e079
Patrick <p@trick-fox.de>
2013-10-27 18:16:33 +0100

    raise version number in tutorial

--- app/models/tutorial/tutorial.rb
+++ app/models/tutorial/tutorial.rb
@@ -8,7 +8,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 0.4.0
+# Version: 0.4.1
 #
 # ATTENTION: this file is auto-generated from rules/tutorial.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -79,7 +79,7 @@ class Tutorial::Tutorial
       :version => {
         :major => 0, 
         :minor => 4, 
-        :build => 0, 
+        :build => 1, 
       },
       
       :production_test_weights => {
