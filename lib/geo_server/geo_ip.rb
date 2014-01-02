34800fd31ddd2e2ba8b6abd3e170967fac4a31ba
salange <sascha@5dlab.com>
2014-01-02 14:33:07 +0100

    Rescue from a timeout of the geo service

--- lib/geo_server/geo_ip.rb
+++ lib/geo_server/geo_ip.rb
@@ -6,10 +6,18 @@ module GeoServer
     
     def self.fetch_coords_for_ip(ip)
       unless ip.nil?
-        response = HTTParty.get("http://freegeoip.net/json/#{ip}", {timeout: 6})
-        if response.code == 200
-          return response.parsed_response
-        end
+        
+        begin
+          response = HTTParty.get("http://freegeoip.net/json/#{ip}", {timeout: 6})
+          if response.code == 200
+            return response.parsed_response
+          end
+        
+        rescue Timeout::Error => e
+          Rails.logger.error "ERROR: Could not fetch geocoordinates during signup."
+          return nil
+        end 
+        
       end
       nil
     end
