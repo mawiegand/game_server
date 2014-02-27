16dc70eac0d416cb6540b7ff8515d1e0afed7ca3
Marcel Wiegand <marcel@bluepanel.org>
2014-02-27 18:50:34 +0100

    Change routes for alliance_leader_vote

--- config/routes.rb
+++ config/routes.rb
@@ -71,9 +71,8 @@ GameServer::Application.routes.draw do
           resources :alliance_shouts
           resource  :alliance_reservation
           resources :victory_progresses
+          resources :alliance_leader_votes
         end
-        
-        resources :alliance_leader_votes, :only => [:create, :update, :delete]
         
         resources :victory_progresses 
         resources :victory_progress_leaders, :only => [ :index ]
