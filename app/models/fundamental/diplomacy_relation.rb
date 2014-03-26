10acdb852bb9c6983438668c44e48a64d6578e2b
Marcel Wiegand <marcel@bluepanel.org>
2014-03-26 19:50:07 +0100

    WAD-94 scopes for Fundamental::DiplomacyRelation

--- app/models/fundamental/diplomacy_relation.rb
+++ app/models/fundamental/diplomacy_relation.rb
@@ -7,4 +7,11 @@ class Fundamental::DiplomacyRelation < ActiveRecord::Base
 
   belongs_to :source_alliance, :class_name => "Fundamental::Alliance", :foreign_key => "source_alliance_id", :inverse_of => :diplomacy_source_relations
   belongs_to :target_alliance, :class_name => "Fundamental::Alliance", :foreign_key => "target_alliance_id", :inverse_of => :diplomacy_target_relations
+  
+  scope :neutral, where(diplomacy_status: RELATION_TYPE_NEUTRAL)
+  scope :ultimatum, where(diplomacy_status: RELATION_TYPE_ULTIMATUM)
+  scope :war, where(diplomacy_status: RELATION_TYPE_WAR)
+  scope :surrender, where(diplomacy_status: RELATION_TYPE_SURRENDER)
+  scope :occupation, where(diplomacy_status: RELATION_TYPE_OCCUPATION)
+  
 end
