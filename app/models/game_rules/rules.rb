34adc5c6ef2031e30290c289201f98cf794f0cae
Marcel Wiegand <marcel@bluepanel.org>
2014-03-28 16:07:42 +0100

    WAD-94 Rules for diplomacy relations

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 1.6.23
+# Version: 1.7.0
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -122,8 +122,8 @@ class GameRules::Rules
     @the_rules ||= GameRules::Rules.new(
   
       :version => { :major => 1,
-                    :minor => 6,
-                    :build => 23,
+                    :minor => 7,
+                    :build => 0,
       },
       :app_control => {
         :debug_tracking                         => 1,
