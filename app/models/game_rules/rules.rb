79ebc503d883d0521bd4946fed749148c8168c4c
jkegel <Jan@5dlab.com>
2014-07-04 20:27:08 +0200

    SP bonus

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 1.6.24
+# Version: 1.6.25
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -123,7 +123,7 @@ class GameRules::Rules
   
       :version => { :major => 1,
                     :minor => 6,
-                    :build => 24,
+                    :build => 25,
       },
       :app_control => {
         :debug_tracking                         => 1,
@@ -8835,7 +8835,7 @@ class GameRules::Rules
           {              #  0
             :id          => 0,
             :exp         => 0,
-            :settlement_points   => 1,
+            :settlement_points   => 999,
             :minimum_sacred_rank => 0,
             :name        => {
               
