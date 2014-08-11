d1a8e75f629001d71691c6c24e3adc9c5e17ddb9
salange <sascha@5dlab.com>
2014-08-11 20:13:13 +0200

    initial version of the tracker

--- lib/five_d.rb
+++ lib/five_d.rb
@@ -1,2 +1,4 @@
+require 'five_d/event_tracker'
+
 module FiveD
 end
---
+++ lib/five_d/event_tracker.rb
@@ -0,0 +1,12 @@
+require "sampl"
+
+module FiveD
+  
+  class EventTracker
+    include Sampl
+     
+    app_token 'wad-rt82-fhjk-18'
+    
+
+  end
+end
