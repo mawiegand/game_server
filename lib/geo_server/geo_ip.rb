3f6e6c98e1994d7c8a00664de098cedece9552e3
salange <sascha@5dlab.com>
2014-11-08 23:17:46 +0100

    rescue more errors

--- lib/geo_server/geo_ip.rb
+++ lib/geo_server/geo_ip.rb
@@ -5,22 +5,25 @@ module GeoServer
   class GeoIp
     
     def self.fetch_coords_for_ip(ip)
-      
-      return nil # presently the geo server is down!
-      
+            
       unless ip.nil?
         
         begin
-          response = HTTParty.get("http://geoip.psiori.com/api/v01/ip/#{ip}", {timeout: 3})
+          response = HTTParty.get("http://geoip.psiori.com/api/v01/ip/#{ip}.json", {timeout: 3})
+          Rails.logger.debug "Code: #{response.code}."
           if response.code == 200
             return response.parsed_response
           end
         
         rescue Timeout::Error => e
-          Rails.logger.error "ERROR: Could not fetch geocoordinates during signup."
+          Rails.logger.error "ERROR: Could not fetch geocoordinates during signup in time."
           Backend::StatusMailer.send_ip_resolution_alert(ip).deliver  # relies on the presence of the mailer; should be solved in a different fashion
-          
           return nil
+
+        rescue 
+          Rails.logger.error "ERROR: Could not fetch geocoordinates during signup due to an unkown error."
+          Backend::StatusMailer.send_ip_resolution_alert(ip).deliver  # relies on the presence of the mailer; should be solved in a different fashion
+          return nil          
         end 
         
       end
