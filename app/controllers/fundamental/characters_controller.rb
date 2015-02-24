bcbc130e2db7085782579193a03b050a7c101ccc
Jona Boeddinghaus <jona@5dlab.com>
2015-02-24 21:27:01 +0100

    retention bonus for returning users

--- app/controllers/fundamental/characters_controller.rb
+++ app/controllers/fundamental/characters_controller.rb
@@ -324,7 +324,7 @@ class Fundamental::CharactersController < ApplicationController
       current_character.last_login_at = DateTime.now
       current_character.increment(:login_count)
       if current_character.can_redeem_retention_bonus_at.nil?
-        current_character.set_can_redeem_retention_bonus_at_login
+        current_character.set_can_redeem_retention_bonus_at
       end
       current_character.save
       
--- app/models/fundamental/character.rb
+++ app/models/fundamental/character.rb
@@ -666,14 +666,8 @@ class Fundamental::Character < ActiveRecord::Base
     return false     if displayed_at.nil?
     return displayed_at < self.created_at + 1.days
   end
-  
+    
   def set_can_redeem_retention_bonus_at
-    self.can_redeem_retention_bonus_start_time = self.created_at
-    #self.can_redeem_retention_bonus_at = (self.created_at + 1.days).change({ hour: 20, min: 0, sec: 0 })    
-    self.can_redeem_retention_bonus_at = self.created_at + 2.minutes
-  end
-  
-  def set_can_redeem_retention_bonus_at_login
     self.can_redeem_retention_bonus_start_time = Time.now
     #self.can_redeem_retention_bonus_at = (Time.now + 1.days).change({ hour: 20, min: 0, sec: 0 })
     self.can_redeem_retention_bonus_at = Time.now + 2.minutes
