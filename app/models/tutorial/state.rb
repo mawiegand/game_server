5db99e23112e3eb79067534442123dcd3f68c4cb
Jona Boeddinghaus <jona@5dlab.com>
2015-07-06 11:16:51 +0200

    quest state nil fix

--- app/models/tutorial/state.rb
+++ app/models/tutorial/state.rb
@@ -93,7 +93,8 @@ class Tutorial::State < ActiveRecord::Base
   
   def completed_tutorial_end_quest?
     self.finished_quests.each do |quest_state|
-      if quest_state.quest[:tutorial_end_quest] unless quest_state.nil?
+      next if quest_state.nil? || quest_state.quest.nil?
+      if quest_state.quest[:tutorial_end_quest]
         return true
       end  
     end
