f6a8add437ff5e4cbfd5d001be0c043b8dbe1ac3
hajorunne <hajo@5dlab.com>
2015-07-17 12:03:07 +0200

    Assignmets 2

--- app/models/game_rules/rules.rb
+++ app/models/game_rules/rules.rb
@@ -9,7 +9,7 @@ require 'active_model'
 # This particular file does hold the following set of rules:
 # Game:    Wack-A-Doo
 # Branch:  development (alpha)
-# Version: 2.3.7
+# Version: 2.3.8
 #
 # ATTENTION: this file is auto-generated from rules/rules.xml . DO NOT EDIT 
 # THIS FILE, as all your edits will be overwritten.
@@ -125,7 +125,7 @@ class GameRules::Rules
   
       :version => { :major => 2,
                     :minor => 3,
-                    :build => 7,
+                    :build => 8,
       },
       :app_control => {
         :debug_tracking                         => 1,
@@ -8299,7 +8299,7 @@ class GameRules::Rules
           },
 
           :costs      => {
-            2 => '0.15*PRODUCTION',
+            2 => '10000',
             
           },
 
