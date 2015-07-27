15c2bda454a9c5a6fa26fecb2d3e28ca4631496c
Marcel Wiegand <marcel@bluepanel.org>
2015-07-27 14:19:37 +0200

    WAD-1232 rename treasures to poacher_treasures

--- app/models/fundamental/treasure.rb
+++ app/models/fundamental/treasure.rb
@@ -1,6 +1,6 @@
 class Fundamental::Treasure < ActiveRecord::Base
 
-  belongs_to :specific_character, :class_name => "Fundamental::Character", :foreign_key => "specific_character_id",    :inverse_of => :treasures
+  belongs_to :specific_character, :class_name => "Fundamental::Character", :foreign_key => "specific_character_id",    :inverse_of => :poacher_treasures
 
   belongs_to :location,           :class_name => "Map::Location",          :foreign_key => "location_id",              :inverse_of => :treasures
   belongs_to :region,             :class_name => "Map::Region",            :foreign_key => "region_id",                :inverse_of => :treasures
