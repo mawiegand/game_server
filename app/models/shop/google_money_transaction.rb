a15c79dd31fcf93fabe47fe72285a1b114b734cc
Daniel Band <dband@outlook.com>
2015-09-18 10:56:46 +0200

    Psiori android tracking purchase fix

--- app/models/shop/google_money_transaction.rb
+++ app/models/shop/google_money_transaction.rb
@@ -14,12 +14,12 @@ class Shop::GoogleMoneyTransaction < ActiveRecord::Base
       user_id:              self.identifier || nil,
       platform:             "android",
       pur_provider:         "google",
-      pur_gross:            0.0,
-      pur_currency:         "de",
-      pur_country_code:     "de",
-      pur_earnings:         0.0 * 0.6,
+      pur_gross:            self.price,
+      pur_currency:         self.currency,
+      pur_earnings:         (self.price.to_f * 0.6).to_s,
       pur_product_sku:      self.google_product_id,
       invoice_id:           self.google_payment_token,
+      pur_receipt_identifier: self.google_payment_token,
       timestamp:            self.created_at || DateTime.now
     });
 
